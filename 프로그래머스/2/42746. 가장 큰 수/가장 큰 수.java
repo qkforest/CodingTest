0e772299d0b374dc281d3d457b49c3d4089fc751
qkforest <89928584+qkforest@users.noreply.github.com>
2023-10-19 16:05:52 +0900

    [level 2] Title: 가장 큰 수, Time: 1.76 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        
+        String[] arr = new String[numbers.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = String.valueOf(numbers[i]);
+        }
+        
+        Arrays.sort(arr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        
+        if(arr[0].equals("0"))
+            answer = "0";
+        else{
+            for (String x: arr)
+                answer = answer + x;
+        }
+        
+        return answer;
+    }
+}
