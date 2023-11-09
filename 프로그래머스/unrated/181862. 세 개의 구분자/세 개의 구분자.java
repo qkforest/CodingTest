bc8decc30116fd7b46cd53289fcbf5ae99c2c9c6
qkforest <89928584+qkforest@users.noreply.github.com>
2023-11-09 15:51:11 +0900

    [level unrated] Title: 세 개의 구분자, Time: 56.70 ms, Memory: 99.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
@@ -4,10 +4,9 @@ class Solution {
     public String[] solution(String myStr) {
         String[] temp = myStr.split("a|b|c");
         List<String> answer = new ArrayList<>();
-        if(temp.length < 1){
-            String[] a = {"EMPTY"};
-            return a;
-        } else {
+        if(temp.length < 1)
+            return new String[] {"EMPTY"};
+        else {
             for(String s : temp){
                 if(!s.equals(""))
                     answer.add(s);
