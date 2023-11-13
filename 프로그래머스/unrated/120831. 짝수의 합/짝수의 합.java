cba7b417a4fb7b38b551fd771ecc389d0078404c
qkforest <89928584+qkforest@users.noreply.github.com>
2023-11-13 10:35:28 +0900

    [level unrated] Title: 짝수의 합, Time: 0.02 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        for(int i = 0; i <= n/2; i++)
+        for(int i = 1; i <= n/2; i++)
             answer += 2*i;
         return answer;
     }
