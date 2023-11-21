99aea66a7a1aee799217a5e656225d3c2751a768
qkforest <89928584+qkforest@users.noreply.github.com>
2023-11-22 00:16:55 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.06 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Solution {
+    public long solution(long n) {
+        long answer = -1;
+        if(n % Math.sqrt(n) == 0)
+            answer = (long)Math.pow(Math.sqrt(n)+1, 2);
+        return answer;
+    }
+}
