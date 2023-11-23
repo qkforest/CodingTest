716ad233c75b446c819d0be68c43986c643798f3
qkforest <89928584+qkforest@users.noreply.github.com>
2023-11-23 12:37:25 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 5.45 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        for(int i = 1; i <= m; i++){
+            if(n % i == 0 && m % i == 0)
+                answer[0] = i;
+        }
+        for(int i = m; i < 1000000; i++){
+            if(i % n == 0 && i % m == 0){
+                answer[1] = i;
+                break;
+            }
+        }
+        return answer;
+    }
+}
