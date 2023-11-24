77d55b29c8a6fe8e50d54306e68026218b098021
qkforest <89928584+qkforest@users.noreply.github.com>
2023-11-24 22:14:41 +0900

    [level 1] Title: 2016년, Time: 0.02 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,8 +1,8 @@
 class Solution {
     public String solution(int a, int b) {
-        String[] week = {"SUN", "MON", "TUE", "WED","THU", "FRI", "SAT"};
+        String[] week = {"MON", "TUE", "WED","THU", "FRI", "SAT", "SUN"};
         int[] days = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        int d = 4 + b;
+        int d = 3 + b;
         for(int i = 1; i < a; i++)
             d += days[i];
         return week[d%7];
