dab0ab0228ef3f1c69dce9c60fc8dc7697d31256
qkforest <89928584+qkforest@users.noreply.github.com>
2023-11-24 13:35:07 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 32.82 ms, Memory: 68.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] numbers) {
+        Set<Integer> answer = new TreeSet<>();
+        for(int i = 0; i < numbers.length-1; i++){
+            for(int j = i+1; j < numbers.length; j++){
+                answer.add(numbers[i]+numbers[j]);
+            }
+        }
+        return Arrays.stream(answer.toArray(new Integer[0])).mapToInt(Integer::intValue).toArray();
+    }
+}
