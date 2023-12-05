e7fbbc019a7f3e670f4d1cc91c2721d3dd95a213
qkforest <89928584+qkforest@users.noreply.github.com>
2023-12-05 23:04:38 +0900

    [level 2] Title: n^2 배열 자르기, Time: 50.32 ms, Memory: 97.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n, long left, long right) {
+        int[] answer = new int[(int)(right-left+1)];
+        long start = 0, first = 0, count = 0, add = 0;
+        int idx = 0;
+        if(left % n == 0)
+            start = left;
+        else
+            start = left - (left % n);
+        for(long i = start; i <= right; i++){
+            if(i % n == 0){
+                first = i/n + 1;
+                count = 1;
+                add = 1;
+            }
+            if(count <= first){
+                if(i >= left){
+                    answer[idx] = (int)first;
+                    idx++;
+                }
+                count++;
+            } else{
+                if(i >= left){
+                    answer[idx] = (int)(first + add);
+                    idx++;
+                }
+                add++;
+            }
+        }  
+        return answer;
+    }
+}
