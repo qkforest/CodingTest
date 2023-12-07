fa2b37f71b01a877cabcca0765a2a4f4fc5198ac
qkforest <89928584+qkforest@users.noreply.github.com>
2023-12-07 15:14:26 +0900

    [level 2] Title: 피로도, Time: 0.04 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int answer;
+    public boolean[] visited;
+    public void dfs(int depth, int k, int[][] dungeons) {
+        for(int i = 0; i < dungeons.length; i++){
+            if(!visited[i] & dungeons[i][0] <= k){
+                visited[i] = true;
+                dfs(depth + 1, k - dungeons[i][1], dungeons);
+                visited[i] = false;
+            }
+        }
+        answer = Math.max(answer, depth);
+    }
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        dfs(0, k, dungeons);
+        return answer;
+    }
+}
