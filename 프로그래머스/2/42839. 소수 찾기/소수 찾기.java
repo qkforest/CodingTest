c670f9cd4e77fde4a49308d295bc6f4763d34f2d
qkforest <89928584+qkforest@users.noreply.github.com>
2024-02-03 17:11:03 +0900

    [level 2] Title: 소수 찾기, Time: 12.12 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,40 +1,44 @@
 import java.util.*;
 class Solution {
-    List<Integer> list;
-    boolean[] visited;
-    int size;
-    char[] num, temp;
+    public static char[] numArray, temp;
+    public static boolean[] visited;
+    public static List<Integer> answer;
+    public static int len;
+    
     public int solution(String numbers) {
-        int answer = 0;
-        num = numbers.toCharArray();
-        size = numbers.length();
-        list = new ArrayList<>();
-        visited = new boolean[size];
-        temp = new char[size];
-        for(int i = 1; i <= size; i++) dfs(0, i);
-        return list.size();
+        numArray = numbers.toCharArray();
+        answer = new ArrayList<>();
+        len = numArray.length;
+        temp = new char[len];
+        visited = new boolean[len];
+        
+        for(int i = 1; i <= len; i++) dfs(0, i);
+        
+        return answer.size();
     }
-    public void dfs(int lvl, int len){
-        if(lvl == len){
-            String t = "";
-            for(char a: temp){
-                if(a != 0) t += a;
+    
+    public void dfs(int level, int size){
+        if(level == size){
+            String s = "";
+            for(char c : temp){
+                if(c != 0) s += c;
             }
-            int n = Integer.parseInt(t);
-            if(!list.contains(n) && n > 1){
-                if(check(n)) list.add(n);
+            int n = Integer.parseInt(s);
+            if(!answer.contains(n) && n > 1){
+                if(check(n)) answer.add(n);
             }
-        } else{
-            for(int i = 0; i < size; i++) {
+        } else {
+            for(int i = 0; i < len; i++){
                 if(!visited[i]){
                     visited[i] = true;
-                    temp[lvl] = num[i];
-                    dfs(lvl+1, len);
+                    temp[level] = numArray[i];
+                    dfs(level+1, size);
                     visited[i] = false;
                 }
             }
         }
     }
+    
     public boolean check(int n){
         for(int i = 2; i < n; i++){
             if(n % i == 0) return false;
