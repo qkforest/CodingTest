020685f120ffb3d33a74eae62903d7d57b356a79
qkforest <89928584+qkforest@users.noreply.github.com>
2024-02-18 18:19:51 +0900

    [level 2] Title: 문자열 압축, Time: 14.63 ms, Memory: 71.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -0,0 +1,44 @@
+import java.util.*;
+class Solution {
+    public int solution(String s) {
+        int len = s.length();
+        int answer = len;
+        /*List<Integer> sizes = new ArrayList<>();
+        for(int i = 1; i < len; i++){
+            if(len % i == 0)
+                sizes.add(i);
+        }
+        System.out.println(sizes);*/
+        for(int size = 1; size < len-1; size++){
+            String pre = s.substring(0, size);
+            String temp = "";
+            int start = size;
+            int count = 1;
+            while(start < len){
+                int end = start+size;
+                if(end > len)
+                    end = len;                   
+                String now = s.substring(start, end);
+                if(pre.equals(now)){
+                    count++;   
+                }else{
+                    if(count == 1){
+                        temp += pre;
+                    }else{
+                        temp += (String.valueOf(count)+pre);
+                    }
+                    pre = now;
+                    count = 1;
+                }
+                start += size;
+            }
+            if(count > 1)
+                temp += (String.valueOf(count)+pre);
+            else
+                 temp += pre;
+            if(temp.length() < answer)
+                answer = temp.length();
+        }
+        return answer;
+    }
+}
