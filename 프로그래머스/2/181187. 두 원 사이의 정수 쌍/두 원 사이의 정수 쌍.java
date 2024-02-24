546960985d91a094da964f95cde94cc6a28baa65
qkforest <89928584+qkforest@users.noreply.github.com>
2024-02-25 00:33:17 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 14.31 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -1,20 +1,19 @@
-import java.util.*;
 class Solution {
     public long solution(int r1, int r2) {
-        long answer = 0;
-        long r1x = (long)r1*r1;
-        long r2x = (long)r2*r2;
-        double y1, y2;
-        for(int i = 0; i < r2; i++){
-            y2 = Math.floor(Math.sqrt(r2x - (long)i*i));
-            if(i >= r1)
-                y1 = 1;
-            else
-                y1 = Math.ceil(Math.sqrt(r1x - (long)i*i));
-            if(y1 == 0)
-                y1++;
-            answer += y2 - y1 + 1;
+        long answer = r2-r1+1;
+        for(int i = 1; i <= r1; i++){
+            double y1 = Math.sqrt(r1*r1 - i*i);
+            double y2 = Math.sqrt(r2*r2 - i*i);
+            if(y2 == (int)y2)
+                y2 += 1.0;
+            answer += (int)y2 - (int)y1;
         }
-        return answer*4;
+        for(int i = r1 + 1; i < r2; i++){
+            double y2 = Math.sqrt(r2*r2 - i*i);
+            if(y2 == (int)y2)
+                y2 += 1.0;
+            answer += (int)y2;
+        }
+        return 4*answer;
     }
 }
