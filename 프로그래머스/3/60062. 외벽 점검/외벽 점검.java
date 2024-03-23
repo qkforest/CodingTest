c15e0100018e5bfd8fef869fc8cd60ae8d68078b
qkforest <89928584+qkforest@users.noreply.github.com>
2024-03-23 23:45:31 +0900

    [level 3] Title: 외벽 점검, Time: 34.20 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/3/60062. 외벽 점검/외벽 점검.java
+++ 프로그래머스/3/60062. 외벽 점검/외벽 점검.java
@@ -3,13 +3,13 @@ class Solution {
     static int answer, len;
     static int[] Weak;
     public int solution(int n, int[] weak, int[] dist) {
-        len = weak.length;
         answer = Integer.MAX_VALUE;
-        Weak = new int[len*2];
+        len = weak.length;
+        Weak = new int[2*len];
         int idx = 0;
         while(idx < len){
             Weak[idx] = weak[idx];
-            Weak[idx+len] = weak[idx++] + n;
+            Weak[len+idx] = weak[idx++]+n;
         }
         for(int i = 0; i < len; i++)
             dfs(i, 0, dist, new boolean[dist.length], new int[dist.length]);
@@ -17,28 +17,28 @@ class Solution {
             return -1;
         return answer;
     }
-    public void dfs(int s, int level, int[] dist, boolean[] visited, int[] permuted){
+    public void dfs(int start, int level, int[] dist, boolean[] visited, int[] per){
         if(level == dist.length){
-            answer = Math.min(answer, check(s, s+len, permuted));
+            answer = Math.min(answer, check(start, start+len, per));
             return;
-        } 
+        }
         for(int i = 0; i < dist.length; i++){
             if(!visited[i]){
                 visited[i] = true;
-                permuted[level] = dist[i];
-                dfs(s, level+1, dist, visited, permuted);
+                per[level] = dist[i];
+                dfs(start, level+1, dist, visited, per);
                 visited[i] = false;
             }
         }
     }
-    public int check(int s, int e, int[] permuted){
+    public int check(int start, int end, int[] per){
         int count = 1;
-        int pos = Weak[s]+permuted[count-1];
-        for(int i = s; i < e; i++){
+        int pos = Weak[start] + per[count-1];
+        for(int i = start; i < end ;i++){
             if(pos < Weak[i]){
                 count++;
-                if(count > permuted.length) return Integer.MAX_VALUE;
-                pos = Weak[i] + permuted[count-1];
+                if(count > per.length) return Integer.MAX_VALUE;
+                pos = Weak[i] + per[count-1];
             }
         }
         return count;
