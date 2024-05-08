e8bf76f93c41780ca7226245a7eb835d52e2520f
qkforest <89928584+qkforest@users.noreply.github.com>
2024-05-09 01:09:54 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 132 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,57 +1,22 @@
 import java.io.*;
-import java.util.*;
 public class Main {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String s = br.readLine();
-		List<String> list = new ArrayList<>();
-		String temp = "";
-		for(char c : s.toCharArray()) {
-			if(isNumber(c)) {
-				temp += c;
-			} else {
-				list.add(temp);
-				temp = "";
-				list.add("" + c);
-			}
-		}
-		list.add(temp);
-		Stack<String> stack = new Stack<>();
-		int idx = 0;
-		while(idx < list.size()) {
-			if(list.get(idx).equals("+")) {
-				int a = Integer.parseInt(stack.pop());
-				idx++;
-				int b = Integer.parseInt(list.get(idx));
-				stack.push(String.valueOf(a+b));
-			} else {
-				stack.push(list.get(idx));
-			}
-			idx++;
-		}
-		if(stack.size() == 1) {
-			System.out.println(stack.pop());
-		} else {
-			Stack<String> stack2 = new Stack<>();
-			while(!stack.isEmpty()) {
-				stack2.push(stack.pop());
-			}
-			int answer = Integer.parseInt(stack2.pop());
-			while(!stack2.isEmpty()) {
-				String now = stack2.pop();
-				if(now.equals("-")) {
-					continue;
-				} else {
-					answer = answer - Integer.parseInt(now);
+		String[] s = br.readLine().split("-");
+		for(int i = 0; i < s.length; i++) {
+			if(s[i].contains("+")) {
+				String[] temp = s[i].split("\\+");
+				int sum = 0;
+				for(String t : temp) {
+					sum += Integer.parseInt(t);
 				}
-			}
-			System.out.println(answer);
+				s[i] = String.valueOf(sum);
+			} 
 		}
-	}
-	public static boolean isNumber(char c) {
-		if(c >= 48 && c <= 57){
-			return true;
+		int answer =Integer.parseInt(s[0]);
+		for(int i = 1; i < s.length; i++) {
+			answer -= Integer.parseInt(s[i]);
 		}
-		return false;
+		System.out.print(answer);
 	}
 }
