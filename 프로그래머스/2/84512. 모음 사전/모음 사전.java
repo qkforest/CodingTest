ec8f925d5bc75fa1cef7f4cb57cc5a60a13b3e84
qkforest <89928584+qkforest@users.noreply.github.com>
2024-05-28 16:09:41 +0900

    [level 2] Title: 모음 사전, Time: 2.91 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,24 +1,26 @@
 import java.util.*;
 class Solution {
-    static char[] WORD = {'A', 'E', 'I', 'O', 'U'};
+    public String[] alphabets = {"A", "E", "I", "O", "U"};
+    public List<String> list;
     public int solution(String word) {
+        list = new ArrayList<>();
         int answer = 0;
-        List<String> dic = new ArrayList<>();
-        for(int i = 0; i < 5; i++)
-            dfs(dic, String.valueOf(WORD[i]));
-        for(int i=0; i<dic.size(); i++){
-            if(dic.get(i).equals(word)){
-                answer = i + 1;
+        dfs("", 0);
+        for(int i = 0; i < list.size(); i++) {
+            if(list.get(i).equals(word)){
+                answer = i;
                 break;
             }
         }
         return answer;
     }
-    public void dfs(List<String> dic, String w){
-        if(w.length() > 5) return;
-        if(!dic.contains(w))
-            dic.add(w);
-        for(int i = 0; i < 5; i++)
-            dfs(dic, w+WORD[i]);
+    public void dfs(String s, int len) {
+        list.add(s);
+        if(len == 5){
+            return;
+        }
+        for(int i = 0; i < 5; i++) {
+            dfs(s+alphabets[i], len+1);
+        }
     }
 }
