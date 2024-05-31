089266be91fd21378f1942f009a06c5157d1e797
qkforest <89928584+qkforest@users.noreply.github.com>
2024-06-01 00:27:51 +0900

    [level 3] Title: 여행경로, Time: 0.03 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,39 +1,41 @@
 import java.util.*;
 class Solution {
+    public String[] answer;
     public boolean[] visited;
-    public String[] answer, temp;
-    public int count, i;
+    public int len;
     public String[] solution(String[][] tickets) {
-        visited = new boolean[tickets.length];
-        count = tickets.length+1;
-        i = 0;
-        Arrays.sort(tickets, (o1, o2) -> o1[1].compareTo(o2[1]));
-        temp = new String[count];
+        len = tickets.length;
+        answer = new String[len+1];
+        visited = new boolean[len];
+        String[] temp = new String[len+1];
         temp[0] = "ICN";
-        dfs(1, "ICN", tickets);
+        dfs(1, "ICN",  temp, tickets);
         return answer;
     }
-    public void dfs(int level, String start, String[][] tickets){
-        boolean r = true;
-        if(level == count){
-            if(i == 0){
-                answer = Arrays.copyOf(temp, count);
-                i++;
+    public void dfs(int level, String from, String[] temp, String[][] tickets) {
+        if(level == len+1) {
+            if(answer[0] == null) {
+                answer = temp.clone();
             }
-            return;
-        }else{
-            for(int i = 0; i < tickets.length; i++){
-                if(start.equals(tickets[i][0])){
-                    if(!visited[i]){
-                        r = false;
-                        visited[i] = true;
-                        temp[level] = tickets[i][1];
-                        dfs(level+1, tickets[i][1], tickets);
-                        visited[i] = false;
+            else{
+                for(int i = 0; i < len; i++) {
+                    if(temp[i].compareTo(answer[i]) < 0) {
+                        answer = temp.clone();
+                        break;
+                    } else if(answer[i].compareTo(temp[i]) < 0) {
+                        break;
                     }
                 }
-           }
-           if(r) return;
-       }
+            }
+            return;
+        }
+        for(int i = 0; i < len; i++) {
+            if(tickets[i][0].equals(from) && !visited[i]) {
+                visited[i] = true;
+                temp[level] = tickets[i][1];
+                dfs(level+1, tickets[i][1], temp, tickets);
+                visited[i] = false;
+            }
+        }
     }
 }
