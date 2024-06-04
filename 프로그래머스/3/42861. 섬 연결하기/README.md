cc6349eb8a5acdd18201db1cdc1b90a05aeb1b33
qkforest <89928584+qkforest@users.noreply.github.com>
2024-06-04 23:42:51 +0900

    [level 3] Title: 섬 연결하기, Time: 0.55 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/README.md
+++ 프로그래머스/3/42861. 섬 연결하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 섬 연결하기 - 42861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861) 
 
 ### 성능 요약
 
-메모리: 73.3 MB, 시간: 0.69 ms
+메모리: 77.5 MB, 시간: 0.55 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 3일 12:34:19
+2024년 06월 04일 23:42:48
 
 ### 문제 설명
 
--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,29 +1,38 @@
 import java.util.*;
 class Solution {
+    public int[] parents;
     public int solution(int n, int[][] costs) {
         int answer = 0;
-        int[] node = new int[n];
-        for(int i = 0; i < n; i++){
-            node[i] = i;
+        parents = new int[n];
+        for(int i = 0; i < n; i++) {
+            parents[i] = i;
         }
-        Arrays.sort(costs, (o1, o2) -> o1[2] - o2[2]);
-        for(int i = 0; i < costs.length; i++){
-            if(head(node, costs[i][0]) != head(node, costs[i][1])){
-                construct(node, costs[i][0], costs[i][1]);
+        Arrays.sort(costs, (a, b) -> a[2] - b[2]);
+        for(int i = 0; i < costs.length; i++) {
+            if(union(costs[i][0], costs[i][1])){
                 answer += costs[i][2];
             }
         }
         return answer;
     }
-    public int head(int[] node, int h){
-        if(node[h] == h) 
-            return h;
-        return 
-            head(node, node[h]);
+    public boolean union(int a, int b){
+        int aParent = find(a);
+        int bParent = find(b);
+        if(aParent == bParent) {
+            return false;
+        }
+        if(aParent < bParent) {
+            parents[bParent] = aParent;
+        } 
+        else {
+            parents[aParent] = bParent;
+        }
+        return true;
     }
-    public void construct(int[] node, int n1, int n2){
-        int first = head(node, n1);
-        int second = head(node, n2);
-        node[first] = second;
+    public int find(int n) {
+        if(parents[n] == n) {
+            return n;
+        }
+        return parents[n] = find(parents[n]);
     }
 }
