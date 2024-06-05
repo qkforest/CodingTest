dbd46a78211f193aac92b46146d9ac5a62594cc9
qkforest <89928584+qkforest@users.noreply.github.com>
2024-06-05 21:16:57 +0900

    [level 3] Title: 단속카메라, Time: 10.93 ms, Memory: 58.3 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,18 +1,16 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] routes) {
-        int answer = 0;
-        Arrays.sort(routes, (o1, o2) ->{return o1[1] - o2[1];});
-        Queue<int[]> cam = new LinkedList<>();
-        for(int[] r : routes) cam.offer(r);
-        while(!cam.isEmpty()){
-            int r = cam.poll()[1];
-            answer++;
-            while(!cam.isEmpty()){
-                int[] t = cam.peek(); 
-                if(t[0] <= r && t[1] >= r) cam.poll();
-                else break;
+        int answer = 1;
+        Arrays.sort(routes, (a, b) -> a[1] - b[1]);
+        int camera = routes[0][1];
+        //System.out.println(Arrays.deepToString(routes));
+        for(int i = 1; i < routes.length; i++) {
+            if(routes[i][0] <= camera) {
+                continue;
             }
+            camera = routes[i][1];
+            answer++;
         }
         return answer;
     }
