3af9fbbafb9489484d9e1342631e6b7ca96d4066
qkforest <89928584+qkforest@users.noreply.github.com>
2024-06-08 00:48:12 +0900

    [level 3] Title: 정수 삼각형, Time: 8.91 ms, Memory: 60.8 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -4,6 +4,7 @@ class Solution {
         for(int level = 1; level < triangle.length; level++){
             triangle[level][0] += triangle[level-1][0];
             triangle[level][triangle[level].length-1] += triangle[level-1][triangle[level-1].length-1];
+             answer = Math.max(triangle[level][triangle[level].length-1], triangle[level][0]);
             for(int idx = 1; idx < triangle[level].length-1; idx++){
                 triangle[level][idx] += Math.max(triangle[level-1][idx-1], triangle[level-1][idx]);
                 answer = Math.max(answer, triangle[level][idx]);
