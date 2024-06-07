52ebe7fc8ca2203bff7b1ebf621f61b1391af3fe
qkforest <89928584+qkforest@users.noreply.github.com>
2024-06-07 10:31:12 +0900

    [level 3] Title: N으로 표현, Time: 0.14 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,29 +1,27 @@
 import java.util.*;
 class Solution {
     public int solution(int N, int number) {
-        HashSet<Integer>[] set = new HashSet[8];
-        for(int i = 0; i < 8; i++) {
+        Set<Integer>[] set = new HashSet[9];
+        for(int i = 1; i <= 8; i++) {
             set[i] = new HashSet<Integer>();
-            String temp = String.valueOf(N).repeat(i+1);
-            set[i].add(Integer.parseInt(temp));
-            for(int j = 0; j < i; j++) {
-                for(int a : set[j]) {
-                    for(int b : set[i - j - 1]) {
-                        set[i].add(a + b);
-                        set[i].add(a - b);
-                        set[i].add(b - a);
-                        set[i].add(a * b);
-                        if(b != 0) {
-                            set[i].add(a / b);
-                        }
+            set[i].add(Integer.parseInt(String.valueOf(N).repeat(i)));
+            for(int j = 1; j < i; j++) {
+                for(int a : set[j]){
+                    for(int b : set[i-j]) {
+                        set[i].add(a-b);
+                        set[i].add(a+b);
+                        set[i].add(a*b);
                         if(a != 0) {
-                            set[i].add(b / a);
+                            set[i].add(b/a);
+                        }
+                        if(b != 0) {
+                            set[i].add(a/b);
                         }
                     }
                 }
             }
-            if(set[i].contains(number)) {
-                return i+1;
+            if(set[i].contains(number)){
+                return i;
             }
         }
         return -1;
