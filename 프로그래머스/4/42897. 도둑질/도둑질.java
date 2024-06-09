45a637cb4d4ced168e896abda699c77dd4e5dfa3
qkforest <89928584+qkforest@users.noreply.github.com>
2024-06-09 17:51:17 +0900

    [level 4] Title: 도둑질, Time: 24.88 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -1,17 +1,15 @@
-import java.util.*;
 class Solution {
-    public long solution(int[] money) {
+    public int solution(int[] money) {
         int len = money.length;
-        long[] dp = new long[len];
+        long[] dp1 = new long[len];
         long[] dp2 = new long[len];
-        dp[0] = 0;
-        dp[1] = money[1];
-        for(int i = 2; i < len; i++)
-            dp[i] = Math.max(dp[i-2]+money[i], dp[i-1]);
-        dp2[0] = 0;
-        dp2[1] = money[0];
-        for(int i = 2; i < len; i++)
-            dp2[i] = Math.max(dp2[i-2]+money[i-1], dp2[i-1]);
-        return Math.max(dp[len-1], dp2[len-1]);
+        dp1[1] = money[0];
+        dp2[1] = money[1];
+         for(int i = 2; i < len; i++) {
+            dp1[i] = Math.max(dp1[i-2] + money[i-1], dp1[i-1]);
+            dp2[i] = Math.max(dp2[i-2] + money[i], dp2[i-1]);
+        }
+        int answer = (int)(Math.max(dp1[len-1], dp2[len-1]));
+        return answer;
     }
 }
