b9259d1a3578d5b5cc3365a8e6cd2ed113294272
qkforest <89928584+qkforest@users.noreply.github.com>
2024-06-23 14:48:24 +0900

    Time: 38 ms (42.68%), Space: 57.9 MB (66.26%) - LeetHub

---
+++ LeetCode/2762-continuous-subarrays/2762-continuous-subarrays.java
@@ -0,0 +1,20 @@
+class Solution {
+    public long continuousSubarrays(int[] nums) {
+        long answer = 0;
+        int i = 0;
+        int len = nums.length;
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int j = 0; j < len; j++) {
+            map.merge(nums[j], 1, Integer::sum);
+            while (map.lastEntry().getKey() - map.firstEntry().getKey() > 2) {
+                map.merge(nums[i], -1, Integer::sum);
+                if (map.get(nums[i]) == 0) {
+                    map.remove(nums[i]);
+                }
+                i++;
+            }
+            answer += j - i + 1;
+        }
+        return answer;
+    }
+}
