7f6f6f30f19f85b4bbfdcf031ee5a5dfecd1c097
qkforest <89928584+qkforest@users.noreply.github.com>
2024-06-27 19:30:12 +0900

    Time: 170 ms (20.16%), Space: 45.2 MB (58.06%) - LeetHub

--- LeetCode/2662-minimum-cost-of-a-path-with-special-roads/2662-minimum-cost-of-a-path-with-special-roads.java
+++ LeetCode/2662-minimum-cost-of-a-path-with-special-roads/2662-minimum-cost-of-a-path-with-special-roads.java
@@ -2,27 +2,29 @@ class Solution {
     public int minimumCost(int[] start, int[] target, int[][] specialRoads) {
         int answer = Math.abs(target[0] - start[0]) + Math.abs(target[1] - start[1]);
         PriorityQueue<Pos> pq = new PriorityQueue<>();
-        Map<String, Integer> map = new HashMap<>();
+        Map<Pair, Integer> map = new HashMap<>();
+        map.put(new Pair<Integer, Integer>(start[0], start[1]), 0);
         pq.offer(new Pos(start[0], start[1], 0));
         while(!pq.isEmpty()){
             Pos now = pq.poll();
             for(int[] road : specialRoads) {
-                int cost = now.cost + Math.abs(road[0] - now.x) + Math.abs(road[1] - now.y) + road[4];
-                String des = String.valueOf(road[2]) + " " + String.valueOf(road[3]);
-                if(map.containsKey(des) && map.get(des) > cost) {
-                    map.put(des, cost);
-                    pq.offer(new Pos(road[2], road[3], cost));
-                }
-                else if(!map.containsKey(des)) {
-                    map.put(des, cost);
-                    pq.offer(new Pos(road[2], road[3], cost));
+                if(road[4] < answer) {
+                    int cost = now.cost + Math.abs(road[0] - now.x) + Math.abs(road[1] - now.y) + road[4];
+                    Pair des = new Pair<Integer, Integer>(road[2], road[3]);
+                    if(map.containsKey(des) && map.get(des) > cost) {
+                        map.put(des, cost);
+                        pq.offer(new Pos(road[2], road[3], cost));
+                    }
+                    else if(!map.containsKey(des)) {
+                        map.put(des, cost);
+                        pq.offer(new Pos(road[2], road[3], cost));
+                    }
                 }
             }
         }
-        for(String key : map.keySet()) {
+        for(Pair key : map.keySet()) {
             int cost = map.get(key);
-            String[] s = key.split(" ");
-            cost += Math.abs(target[0] - Integer.parseInt(s[0])) + Math.abs(target[1] - Integer.parseInt(s[1]));
+            cost += Math.abs(target[0] - (int)key.getKey()) + Math.abs(target[1] - (int)key.getValue());
             answer = Math.min(cost, answer);
         }
         return answer;
