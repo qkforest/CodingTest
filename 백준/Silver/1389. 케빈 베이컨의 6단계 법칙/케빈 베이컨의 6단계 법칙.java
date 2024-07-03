12f00f61096ff31435ba2450b36da478aa87ecca
qkforest <89928584+qkforest@users.noreply.github.com>
2024-07-03 23:51:11 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 112 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -1,45 +1,39 @@
 import java.io.*;
+import java.util.*;
+import java.util.stream.IntStream;
 class Main {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] s = br.readLine().split(" ");
 		int N = Integer.parseInt(s[0]);
 		int M = Integer.parseInt(s[1]);
-		int[][] cost = new int[N+1][N+1];
-		for(int i = 1; i <= N; i++) {
-			for(int j = 1; j <= N; j++) {
-				cost[i][j] = 100;
-				if(i == j) {
-					cost[i][j] = 0;
-				}
-			}
+		int[][] cost = new int[N][N];
+		int answer = 0;
+		int sum = Integer.MAX_VALUE;
+		for(int i = 0; i < N; i++) {
+			Arrays.fill(cost[i], 100);
+			cost[i][i] = 0;
 		}
 		for(int i = 0; i < M; i++) {
 			s = br.readLine().split(" ");
-			int u = Integer.parseInt(s[0]);
-			int v = Integer.parseInt(s[1]);
+			int u = Integer.parseInt(s[0]) - 1;
+			int v = Integer.parseInt(s[1]) - 1;
 			cost[u][v] = 1;
 			cost[v][u] = 1;
 		}
-		for(int k = 1; k <= N; k++) {
-			for(int i = 1; i <= N; i++) {
-				for(int j = 1; j <= N; j++) {
+		for(int k = 0; k < N; k++) {
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < N; j++) {
 					cost[i][j] = Math.min(cost[i][j], cost[i][k]+cost[k][j]);
 				}
 			}
 		}
-		int temp = Integer.MAX_VALUE;
-		int answer = 1;
-		for(int i = 1; i <= N; i++) {
-			int sum = 0;
-			for(int j = 1; j <= N; j++) {
-				sum += cost[i][j];
+		for(int i = 0; i < N; i++) {
+			int temp = IntStream.of(cost[i]).sum();
+			if(temp < sum) {
+				sum = temp;
+				answer = i+1;
 			}
-			
-			if(temp > sum) {
-				temp = sum;
-				answer = i;
-			}	
 		}
 		System.out.print(answer);
 	}
