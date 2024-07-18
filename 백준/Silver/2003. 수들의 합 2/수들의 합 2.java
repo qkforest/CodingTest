5b79e2da3eef8ece2c437cf0996681146549f1e8
qkforest <89928584+qkforest@users.noreply.github.com>
2024-07-18 10:33:38 +0900

    [Silver IV] Title: 수들의 합 2, Time: 164 ms, Memory: 15632 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -1,37 +1,44 @@
 import java.io.*;
+
 class Main{
-	public static int N, M;
-	public static int[] parents;
-	public static void main(String args[]) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] s = br.readLine().split(" ");
-		N = Integer.parseInt(s[0]);
-		M = Integer.parseInt(s[1]);
-		int answer = 0;
-		s = br.readLine().split(" ");
-		long[] dp = new long[N];
-		dp[0] = Long.parseLong(s[0]);
-		for(int i = 1; i < N; i++) {
-			dp[i] =  dp[i-1] + Long.parseLong(s[i]);
-		}
-		for(int j = 0; j < N; j++) {
+	public static int findCases(int N, int M, long[] sums) {
+		int count = 0;
+		for(int i = 1; i <= N; i++) {
 			int left = 0;
-			int right = j;
+			int right = i;
 			while(left <= right) {
 				int mid = (left + right) / 2;
-				long sum = dp[j] - dp[mid] +  Long.parseLong(s[mid]);
+				long sum = sums[i] - sums[mid];
 				if(sum == M) {
-					answer++;
+					count++;
 					break;
 				}
-				else if(sum  > M) {
-					left = mid +1;
+				else if(sum > M) {
+					left = mid + 1;
 				}
 				else {
 					right = mid - 1;
 				}
 			}
 		}
-		System.out.println(answer);
+		return count;
+	}
+	
+	public static void main(String args[]) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] str = br.readLine().split(" ");
+		int N = Integer.parseInt(str[0]);
+		int M = Integer.parseInt(str[1]);
+		
+		long[] sums = new long[N+1];
+		str = br.readLine().split(" ");
+		sums[1] = Integer.parseInt(str[0]);
+		
+		for(int i = 2; i <= N; i++) {
+			sums[i] = Long.parseLong(str[i-1]) + sums[i-1];
+		}
+		
+		System.out.print(findCases(N, M, sums));
 	}
 }
