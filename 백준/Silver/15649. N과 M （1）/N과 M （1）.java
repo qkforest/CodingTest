4009421225015f48228bb89b97bde1032fb48563
qkforest <89928584+qkforest@users.noreply.github.com>
2024-07-21 21:01:24 +0900

    [Silver III] Title: N과 M (1), Time: 296 ms, Memory: 22924 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,30 +1,36 @@
 import java.io.*;
-import java.util.*;
+
 public class Main {
-	public static int[] answer;
+	public static StringBuilder sb;
 	public static boolean[] visited;
+	public static int[] arr;
 	public static int N, M;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] s = br.readLine().split(" ");
-		N = Integer.parseInt(s[0]);
-		M = Integer.parseInt(s[1]);
-		answer = new int[M];
-		visited = new boolean[N+1];
-		dfs(0);
-	}
 	public static void dfs(int level) {
 		if(level == M) {
-			System.out.println(Arrays.toString(answer).replace("[", "").replace("]", "").replace(",", ""));
+			for(int num : arr) {
+				sb.append(num).append(" ");
+			}
+			sb.deleteCharAt(sb.length()-1).append("\n");
 			return;
 		}
 		for(int i = 1; i <= N; i++) {
 			if(!visited[i]) {
-				answer[level] = i;
+				arr[level] = i;
 				visited[i] = true;
 				dfs(level+1);
 				visited[i] = false;
 			}
 		}
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		String[] s = br.readLine().split(" ");
+		N = Integer.parseInt(s[0]);
+		M = Integer.parseInt(s[1]);
+		visited = new boolean[N+1];
+		arr = new int[M];
+		dfs(0);
+		System.out.print(sb);
 	}
 }
