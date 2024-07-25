b600ba2ccc395ce6c446e58866ece6e48681c6a0
qkforest <89928584+qkforest@users.noreply.github.com>
2024-07-25 14:53:45 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 108 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -1,31 +1,27 @@
 import java.io.*;
 import java.util.*;
-public class Main {
-	public static void main(String[] args) throws IOException{
+
+class Main {
+	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int testcases = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
-		for(int i = 0; i < T; i++) {
-			int N = Integer.parseInt(br.readLine());
-			HashMap<String, List<String>> map = new HashMap<>();
-			for(int j = 0 ; j < N; j++) {
+		
+		for(int t = 0; t < testcases; t++) {
+			Map<String, Integer> map = new HashMap<>();
+			int n = Integer.parseInt(br.readLine());
+			for(int i = 0; i < n; i++) {
 				String[] s = br.readLine().split(" ");
-				List<String> temp;
-				if(map.containsKey(s[1])) {
-					temp = map.get(s[1]);
-				} else {
-					temp = new ArrayList<>();
-				}
-				temp.add(s[0]);
-				map.put(s[1], temp);
+				map.merge(s[1], 1, Integer::sum);
 			}
 			int answer = 1;
-			for(String k : map.keySet()) {
-				List<String> temp = map.get(k);
-				answer *= (temp.size()+1);
+			for(String type : map.keySet()) {
+				answer *= (map.get(type)+1);
 			}
 			sb.append(answer-1).append("\n");
 		}
-		System.out.println(sb);
+		
+		// 출력
+		System.out.print(sb);
 	}
 }
