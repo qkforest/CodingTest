4c1120bff60358c82f7fb99cb27590f1e299ff3c
qkforest <89928584+qkforest@users.noreply.github.com>
2024-07-27 13:50:16 +0900

    [Silver II] Title: 숫자판 점프, Time: 180 ms, Memory: 18760 KB -BaekjoonHub

---
+++ 백준/Silver/2210. 숫자판 점프/숫자판 점프.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static String[][] board;
+	public static int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+	public static Set<String> set;
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		board = new String[5][5];
+		set = new TreeSet<>();
+		for(int i = 0; i < 5; i++) {
+			board[i] = br.readLine().split(" ");
+		}
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j < 5; j++) {
+				bfs(i, j);
+			}
+		}
+		System.out.print(set.size());
+	}
+	public static void bfs(int i, int j) {
+		Queue<Square> q = new LinkedList<>();
+		q.offer(new Square(i, j, 1, board[i][j]));
+		while(!q.isEmpty()) {
+			Square now = q.poll();
+			if(now.length == 6) {
+				set.add(now.s);
+				continue;
+			}
+			for(int[] d : directions) {
+				int y = now.y + d[0];
+				int x = now.x + d[1];
+				if(y >= 0 && y < 5 && x >= 0  && x < 5) {
+					q.offer(new Square(y, x, now.length+1, new StringBuilder(now.s).append(board[y][x]).toString()));
+				}
+			}
+		}
+	}
+		
+	static class Square {
+		int y;
+		int x;
+		int length;
+		String s;
+		
+		public Square(int y, int x, int length, String s) {
+			this.y = y;
+			this.x = x;
+			this.length = length;
+			this.s = s;
+		}
+	}
+}
