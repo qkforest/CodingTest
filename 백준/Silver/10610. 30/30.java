88b8427b2c3c4ea56abd81ac0a7533d6e1ea9ec4
qkforest <89928584+qkforest@users.noreply.github.com>
2024-07-31 16:07:07 +0900

    [Silver IV] Title: 30, Time: 224 ms, Memory: 19980 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.java
+++ 백준/Silver/10610. 30/30.java
@@ -4,24 +4,22 @@ import java.util.*;
 class Main {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		String N = br.readLine();
 		int arr[] = new int[N.length()];
         int sum = 0;
-        
         for (int i = 0; i < N.length(); i++) {
             arr[i] = N.charAt(i) - '0';
             sum += N.charAt(i) - '0';
         }
-        
         Arrays.sort(arr);
-        
-        if (sum % 3 == 0 && arr[0] == 0) {
+        StringBuilder sb = new StringBuilder();
+        if (arr[0] == 0 && sum % 3 == 0)  {
             for (int i = N.length()-1; i >= 0; i--) {
-                System.out.print(arr[i]);
+            	sb.append(arr[i]);
             }
         } else {
-            System.out.println(-1);
+        	 sb.append(-1);
         }
+        System.out.println(sb);
 	}
 }
