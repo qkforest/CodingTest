61da0e4a283581bd413721149a7f3bdb6d287185
qkforest <89928584+qkforest@users.noreply.github.com>
2024-08-02 09:52:39 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 148 ms, Memory: 14548 KB -BaekjoonHub

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -1,54 +1,23 @@
 import java.io.*;
-import java.util.*;
-
 class Main {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		String[] str = br.readLine().split(" ");
-		PriorityQueue<Seq> pq = new PriorityQueue<>();
-		int answer = 1;
-		for(String s : str) {
-			int num = Integer.parseInt(s);
-			List<Seq> temp = new ArrayList<>();
-			boolean newSeq = true;
-			while(!pq.isEmpty()) {
-				Seq pre = pq.poll();
-				temp.add(pre);
-				if(pre.last <= num) {
-					continue;
-				} else {
-					int l = pre.len + 1 ;
-					pq.offer(new Seq(num, l));
-					answer = Math.max(answer, l);
-					newSeq = false;
-					break;
+		int[][] sequence = new int[N][2];
+		for(int i = 0; i < N; i++) {
+			sequence[i][0] = Integer.parseInt(str[i]);
+			sequence[i][1] = 1;
+		}
+		int answer = 0;
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < i; j++) {
+				if(sequence[j][0] > sequence[i][0] && sequence[j][1] + 1 > sequence[i][1]) {
+					sequence[i][1] = sequence[j][1] + 1;
 				}
 			}
-			for(Seq add: temp) {
-				pq.offer(add);
-			}
-			if(newSeq) {
-				pq.offer(new Seq(num, 1));
-			}
+			answer = Math.max(answer, sequence[i][1]);
 		}
-		System.out.print(answer);
-	}
-	static class Seq implements Comparable<Seq> {
-		int last;
-		int len;
-		
-		public Seq(int last, int len) {
-			this.last = last;
-			this.len = len;
-		}
-		
-		@Override
-		public int compareTo(Seq other) {
-			if(this.len == other.len) {
-				return other.last - this.last;
-			}
-			return other.len - this.len;
-		}
+ 		System.out.print(answer);
 	}
 }
