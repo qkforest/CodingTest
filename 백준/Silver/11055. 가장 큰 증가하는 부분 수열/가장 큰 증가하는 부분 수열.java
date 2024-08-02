27cfd548692281dbbaddbfc1b71c27d817a3647e
qkforest <89928584+qkforest@users.noreply.github.com>
2024-08-02 14:00:14 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 144 ms, Memory: 14660 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -1,25 +1,29 @@
 import java.io.*;
+
 class Main {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		String[] str = br.readLine().split(" ");
 		int[][] sequence = new int[N][2];
+		
+		int answer = 0;
+		
 		for(int i = 0; i < N; i++) {
 			int num = Integer.parseInt(str[i]);
 			sequence[i][0] = num;
-			sequence[i][1] = num;
-		}
-		int answer = 0;
-		for(int i = 0; i < N; i++) {
+			sequence[i][1] = num; // 합 저장
 			for(int j = 0; j < i; j++) {
 				int sum = sequence[j][1] + sequence[i][0];
 				if(sequence[j][0] < sequence[i][0] && sum > sequence[i][1]) {
 					sequence[i][1] = sum;
 				}
 			}
-			answer = Math.max(answer, sequence[i][1]);
+			
+			answer = Math.max(answer, sequence[i][1]); // 최대 합 갱신
 		}
+		
+		// 출력
  		System.out.print(answer);
 	}
 }
