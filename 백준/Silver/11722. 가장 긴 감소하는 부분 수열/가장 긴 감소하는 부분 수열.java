4f1cf3eb1ede938484c8f98756544ba17a9b6e94
qkforest <89928584+qkforest@users.noreply.github.com>
2024-08-02 14:02:27 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 144 ms, Memory: 14884 KB -BaekjoonHub

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -1,23 +1,27 @@
 import java.io.*;
+
 class Main {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		String[] str = br.readLine().split(" ");
 		int[][] sequence = new int[N][2];
+		
+		int answer = 0;
+		
 		for(int i = 0; i < N; i++) {
 			sequence[i][0] = Integer.parseInt(str[i]);
-			sequence[i][1] = 1;
-		}
-		int answer = 0;
-		for(int i = 0; i < N; i++) {
+			sequence[i][1] = 1; // 최대 길이
 			for(int j = 0; j < i; j++) {
 				if(sequence[j][0] > sequence[i][0] && sequence[j][1] + 1 > sequence[i][1]) {
 					sequence[i][1] = sequence[j][1] + 1;
 				}
 			}
-			answer = Math.max(answer, sequence[i][1]);
+			
+			answer = Math.max(answer, sequence[i][1]); // 최대 길이 갱신
 		}
+		
+		// 출력
  		System.out.print(answer);
 	}
 }
