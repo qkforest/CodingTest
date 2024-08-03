b532a3cd4500763210022c761cc07bbb8e9d4596
qkforest <89928584+qkforest@users.noreply.github.com>
2024-08-03 11:13:17 +0900

    [Silver IV] Title: 게임을 만든 동준이, Time: 104 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.java
+++ 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.java
@@ -1,21 +1,25 @@
 import java.io.*;
-public class Main {
+import java.util.*;
+
+class Main {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int[] scores = new int[N];
-		for(int i = 0; i < N; i++) {
-			scores[i] = Integer.parseInt(br.readLine());
+		Deque<Integer> stack = new ArrayDeque<>();
+		for(int i = 0;i < N; i++) {
+			stack.push(Integer.parseInt(br.readLine()));
 		}
+		int min = stack.pop();
 		int answer = 0;
-		for(int i = N-2; i >= 0; i--) {
-			if(scores[i] < scores[i+1]) {
-				continue;
+		while(!stack.isEmpty()) {
+			int now = stack.pop();
+			if(now >= min) {
+				answer += (now - min) + 1 ;
+				min--;
 			} else {
-				answer += Math.abs(scores[i+1] - scores[i]) + 1;
-				scores[i] = scores[i+1] - 1;
+				min = now;
 			}
 		}
-		System.out.println(answer);
+		System.out.print(answer);
 	}
 }
