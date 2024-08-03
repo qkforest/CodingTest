ca8265d67f5ac9c6d747aee7e3ce63239bdc0a66
qkforest <89928584+qkforest@users.noreply.github.com>
2024-08-03 11:50:23 +0900

    [Silver IV] Title: 게임을 만든 동준이, Time: 104 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.java
+++ 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.java
@@ -5,21 +5,33 @@ class Main {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
+		
 		Deque<Integer> stack = new ArrayDeque<>();
+		
 		for(int i = 0;i < N; i++) {
 			stack.push(Integer.parseInt(br.readLine()));
 		}
-		int min = stack.pop();
-		int answer = 0;
+		
+		int min = stack.pop(); // 현 시점 최소 점수
+		int answer = 0; // 감소할 점수의 총합
+		
 		while(!stack.isEmpty()) {
-			int now = stack.pop();
+			int now = stack.pop(); // 현 레벨 점수
+			
+			/** 
+			 * 현 레벨 점수보다 최소 점수가 작거나 같다면 
+			 * 현 레벨 점수를 최소 점수보다 1 작은 정수로 만들고 
+			 * answer에 감소할 점수를 더함
+			 */
 			if(now >= min) {
-				answer += (now - min) + 1 ;
 				min--;
+				answer += now - min;
 			} else {
-				min = now;
+				min = now; // 최소 점수를 현 레벨 점수로 갱신
 			}
+			
 		}
+		
 		System.out.print(answer);
 	}
 }
