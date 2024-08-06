c6f8cf457ef52d99028e9aee0b754460849675bc
qkforest <89928584+qkforest@users.noreply.github.com>
2024-08-06 09:39:25 +0900

    [Silver V] Title: 문서 검색, Time: 100 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.java
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -1,24 +1,28 @@
+import java.util.*;
 import java.io.*;
 
 class Main {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder doc = new StringBuilder(br.readLine());
-		String search = br.readLine();
+		String doc = br.readLine(); // 문서
+		String search = br.readLine(); // 검색 단어
 		
-		int start = 0;
-		int end = search.length();
-		int count = 0;
+		int start = 0; // 서브스트링의 시작 인덱스
+		int end = search.length(); // 서브스트링의 끝 인덱스
+		int count = 0; // 중복 없이 등장하는 단어 개수
 		
 		while(start < doc.length() && end <= doc.length()) {
+			int next = start + 1;
 			if(doc.substring(start, end).equals(search)) {
 				count++;
-				start = end;
-				end = start + search.length();
-			} else {
-				start++;
-				end++;
+				next = end;
 			}
+			int idx = doc.substring(next).indexOf(search.charAt(0));
+			if(idx == -1) {
+				break;
+			}
+			start = next + idx;
+			end = start + search.length();
 		}
 		
 		System.out.print(count);
