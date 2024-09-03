51cef4c3d7b762b65eeef132acce3c38efb89a78
qkforest <89928584+qkforest@users.noreply.github.com>
2024-09-03 21:37:35 +0900

    [Silver III] Title: N과 M (3), Time: 476 ms, Memory: 127120 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -4,14 +4,12 @@ class Main {
 	public static StringBuilder sb;
 	public static int[] arr;
 	public static int N, M;
-	public static Set<String> set;	
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		sb = new StringBuilder();
 		String[] s = br.readLine().split(" ");
 		N = Integer.parseInt(s[0]);
 		M = Integer.parseInt(s[1]);
-		set = new HashSet<>();
 		arr = new int[M];
 		
 		dfs(0);
@@ -26,10 +24,7 @@ class Main {
 			for(int num : arr) {
 				temp.append(num).append(" ");
 			}
-			String str = temp.toString();
-			if(set.add(str)) {
-				sb.append(str).append("\n");
-			}
+			sb.append(temp.toString()).append("\n");
 			return;
 		}
 		for(int i = 1; i <= N; i++) {
