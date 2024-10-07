e79919d3be0550aa66292141fc05790df7d3f5c2
qkforest <89928584+qkforest@users.noreply.github.com>
2024-10-07 23:22:53 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 14012 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+class Main {
+	public static void main(String args[]) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		sb.append("\\    /\\").append("\n");
+        sb.append(" )  ( \')").append("\n");
+        sb.append("(  /  )").append("\n");
+        sb.append(" \\(__)|").append("\n");
+		System.out.print(sb);
+	}
+}
