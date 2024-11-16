3b58c4ec5c58b988f23735d1b04096ba749bc66f
qkforest <89928584+qkforest@users.noreply.github.com>
2024-11-16 23:23:36 +0900

    [Bronze II] Title: 숫자, Time: 232 ms, Memory: 24556 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10093. 숫자/숫자.java
+++ 백준/Bronze/10093. 숫자/숫자.java
@@ -3,15 +3,16 @@ class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] s = br.readLine().split(" ");
-		int A = Math.min(Integer.parseInt(s[0]), Integer.parseInt(s[1]));
-		int B = Math.max(Integer.parseInt(s[0]), Integer.parseInt(s[1]));
+		long A = Math.min(Long.parseLong(s[0]), Long.parseLong(s[1]));
+		long B = Math.max(Long.parseLong(s[0]), Long.parseLong(s[1]));
 		StringBuilder sb = new StringBuilder();
 		if(A == B) {
 			sb.append(0);
 		} else {
 			sb.append(B - A - 1).append("\n");
-			for(int i = A+1; i < B; i++) {
-			sb.append(i).append(" ");
+			A++;
+			while(A < B) {
+				sb.append(A++).append(" ");
 			}
 			sb.deleteCharAt(sb.length()-1);
 		}
