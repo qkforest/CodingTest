da2c4ebdb87dd6b86e2e6fcfd9a037502b4fe1c7
qkforest <89928584+qkforest@users.noreply.github.com>
2024-12-05 23:29:35 +0900

    [Bronze V] Title: ??!, Time: 108 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,9 @@
+import java.io.*;
+class Main {
+	public static int[][] arr;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder(br.readLine());
+		System.out.print(sb.append("??!"));
+	}
+}
