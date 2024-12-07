6f9f5d8134037ba0f333f161ffcdbb0920d95f76
qkforest <89928584+qkforest@users.noreply.github.com>
2024-12-07 23:46:48 +0900

    [Bronze V] Title: 개, Time: 100 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		sb.append("|\\_/|").append("\n");
+		sb.append("|q p|   /}").append("\n");
+		sb.append("( 0 )\"\"\"\\").append("\n"); 
+		sb.append("|\"^\"`    |").append("\n");
+		sb.append("||_/=\\\\__|");
+		System.out.print(sb);
+	}
+}
