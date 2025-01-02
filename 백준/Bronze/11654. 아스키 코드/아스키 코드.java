6a5fc88bc191ad1ec52b0b9062516396c7579927
qkforest <89928584+qkforest@users.noreply.github.com>
2025-01-02 16:08:37 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,9 @@
+import java.io.*;
+class Main {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuffer sb = new StringBuffer();
+		char c = br.readLine().charAt(0);
+		System.out.print(c - 0);
+	}
+}
