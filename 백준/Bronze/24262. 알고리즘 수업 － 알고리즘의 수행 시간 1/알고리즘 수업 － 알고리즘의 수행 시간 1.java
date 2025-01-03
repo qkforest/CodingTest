c160c46f800a655a70500740398bb97c33162b7d
qkforest <89928584+qkforest@users.noreply.github.com>
2025-01-03 14:32:34 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 112 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,9 @@
+import java.io.*;
+class Main {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuffer sb = new StringBuffer();
+		int N = Integer.parseInt(br.readLine());
+		System.out.print("1" + "\n" + "0");
+	}
+}
