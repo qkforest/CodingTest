738848ef7040cdde69f2f523e9b9b5dcdb458d78
qkforest <89928584+qkforest@users.noreply.github.com>
2025-01-10 22:43:04 +0900

    [Bronze V] Title: 새싹, Time: 104 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,14 @@
+import java.io.*;
+class Main {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		sb.append("         ,r'\"7").append("\n");
+		sb.append("r`-_   ,'  ,/").append("\n");
+		sb.append(" \\. \". L_r'").append("\n");
+		sb.append("   `~\\/").append("\n");
+		sb.append("      |").append("\n");
+		sb.append("      |").append("\n");
+		System.out.print(sb);
+	}
+}
