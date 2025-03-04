b37967d1ea504705b2d7e8e168e0e265296ad70a
qkforest <89928584+qkforest@users.noreply.github.com>
2025-03-04 17:50:30 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 840 ms, Memory: 147064 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -6,24 +6,20 @@ class Main {
 		String[] s = br.readLine().split(" ");
 		int N = Integer.parseInt(s[0]);
 		int M = Integer.parseInt(s[1]);
-		int[][] arr = new int[N][N+1];
-		for(int i = 0; i < N; i++) {
+		long[][] arr = new long[N+1][N+1];
+		for(int i = 1; i <= N; i++) {
 			s = br.readLine().split(" ");
 			for(int j = 1; j <= N; j++) {
-				arr[i][j] = arr[i][j-1] + Integer.parseInt(s[j-1]);
+				arr[i][j] = arr[i][j-1] + arr[i-1][j] + Integer.parseInt(s[j-1]) - arr[i-1][j-1];
 			}
 		}
 		for(int i = 0; i < M; i++) {
 			s = br.readLine().split(" ");
-			long answer = 0;
-			int x1 = Integer.parseInt(s[0]) - 1;
-			int y1 = Integer.parseInt(s[1]) - 1;
-			int x2 = Integer.parseInt(s[2]) - 1;
+			int x1 = Integer.parseInt(s[0]);
+			int y1 = Integer.parseInt(s[1]);
+			int x2 = Integer.parseInt(s[2]);
 			int y2 = Integer.parseInt(s[3]);
-			for(int j = x1; j <= x2; j++) {
-				answer += arr[j][y2] - arr[j][y1];
-			}
-			sb.append(answer).append("\n");
+			sb.append(arr[x2][y2]-arr[x2][y1-1]-arr[x1-1][y2]+arr[x1-1][y1-1]).append("\n");
 		}
 		System.out.print(sb);
 	}
