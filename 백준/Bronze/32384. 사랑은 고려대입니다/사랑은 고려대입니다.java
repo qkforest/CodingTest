7c4b443082d69490146e47226055f2f60c10ffa7
qkforest <89928584+qkforest@users.noreply.github.com>
2025-04-01 23:32:14 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class Main {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		for(int i = 0; i < N; i++) {
+			sb.append("LoveisKoreaUniversity ");
+		}
+		
+		System.out.print(sb);
+	}
+}
