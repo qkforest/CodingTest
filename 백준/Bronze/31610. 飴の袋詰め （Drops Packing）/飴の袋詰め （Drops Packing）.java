a3a8f5b47a7c6fa7a40a1db86c4531886f2ca52f
qkforest <89928584+qkforest@users.noreply.github.com>
2025-05-15 23:12:14 +0900

    [Bronze V] Title: 飴の袋詰め (Drops Packing), Time: 108 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/31610. 飴の袋詰め （Drops Packing）/飴の袋詰め （Drops Packing）.java
@@ -0,0 +1,9 @@
+import java.io.*;
+class Main {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int answer = Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine());
+		answer += Integer.parseInt(br.readLine());
+        System.out.print(answer);
+	}
+}
