0aa3edac975a4aa9fc3f9f0a027072d5a0b3de98
qkforest <89928584+qkforest@users.noreply.github.com>
2025-05-20 23:22:07 +0900

    [Bronze V] Title: Just Round Down, Time: 100 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Bronze/32458. Just Round Down/Just Round Down.java
@@ -0,0 +1,7 @@
+import java.io.*;
+class Main {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print((int)Math.floor(Double.parseDouble(br.readLine())));
+	}
+}
