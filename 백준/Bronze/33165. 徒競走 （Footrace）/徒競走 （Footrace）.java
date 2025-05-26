912322d3a899307eac537bdcbec6d45fd6dcacfc
qkforest <89928584+qkforest@users.noreply.github.com>
2025-05-26 23:41:22 +0900

    [Bronze V] Title: 徒競走 (Footrace), Time: 124 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/33165. 徒競走 （Footrace）/徒競走 （Footrace）.java
@@ -0,0 +1,7 @@
+import java.io.*;
+class Main {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print(Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine()));
+	}
+}
