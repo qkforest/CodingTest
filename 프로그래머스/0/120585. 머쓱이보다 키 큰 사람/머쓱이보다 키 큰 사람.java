85b3cb89d5e6163035f69989c0b50dba2bccbfe4
qkforest <89928584+qkforest@users.noreply.github.com>
2025-06-26 23:33:26 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.03 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int[] array, int height) {
+        int answer = 0;
+        for(int h : array){
+            if(h > height)
+                answer += 1;
+        }
+        return answer;
+    }
+}
