92763144b0ddbad9a5aeb868b0b3e3b411595e4c
qkforest <89928584+qkforest@users.noreply.github.com>
2025-06-28 23:49:24 +0900

    [level 0] Title: 가위 바위 보, Time: 2.92 ms, Memory: 86.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String rsp) {
+        String answer = "";
+        for(char c: rsp.toCharArray()){
+            switch(c){
+                case '2': 
+                    answer += '0';
+                    break;
+                case '0':
+                    answer += '5';
+                    break;
+                case '5':
+                    answer += '2';
+                    break;
+            }
+        }
+        return answer;
+    }
+}
