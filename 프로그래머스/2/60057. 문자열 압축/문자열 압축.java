391ec8cf23b7938a8f6202fdbb4edd078b5b4696
qkforest <89928584+qkforest@users.noreply.github.com>
2025-07-29 15:04:32 +0900

    [level 2] Title: 문자열 압축, Time: 22.70 ms, Memory: 97 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,37 +1,40 @@
 import java.util.*;
+
 class Solution {
-    public int solution(String s) {
-        int len = s.length();
-        int answer = len;
-        for(int size = 1; size < len-1; size++){
-            String pre = s.substring(0, size);
-            String temp = "";
-            int start = size;
-            int count = 1;
-            while(start < len){
-                int end = start+size;
-                if(end > len)
-                    end = len;                   
-                String now = s.substring(start, end);
-                if(pre.equals(now)){
-                    count++;   
-                }else{
-                    if(count == 1){
-                        temp += pre;
-                    }else{
-                        temp += (String.valueOf(count)+pre);
-                    }
-                    pre = now;
-                    count = 1;
+    private List<String> split(String s, int len) {
+        List<String> tokens = new ArrayList<>();
+        for(int i = 0; i < s.length(); i += len) {
+            int end = Math.min(s.length(), i+len);
+            tokens.add(s.substring(i, end));
+        }
+        return tokens;
+    }
+    private int compressed(String s, int len) {
+        StringBuilder sb = new StringBuilder();
+        String last = "";
+        int c = 0;
+        for(String token : split(s, len)) {
+            if(token.equals(last)) {
+                c++;
+            } else {
+                if(c > 1) {
+                    sb.append(c);
                 }
-                start += size;
+                sb.append(last);
+                last = token;
+                c = 1;
             }
-            if(count > 1)
-                temp += (String.valueOf(count)+pre);
-            else
-                 temp += pre;
-            if(temp.length() < answer)
-                answer = temp.length();
+        }
+        if(c > 1) {
+            sb.append(c);
+        }
+        sb.append(last);
+        return sb.length();
+    }
+    public int solution(String s) {
+        int answer = Integer.MAX_VALUE;
+        for(int i = 1; i <= s.length(); i++) {
+            answer = Math.min(compressed(s, i), answer);
         }
         return answer;
     }
