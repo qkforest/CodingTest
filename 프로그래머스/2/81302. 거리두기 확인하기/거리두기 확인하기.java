e529a43138f6aa1491fd6ca3ca77e1dabede0f9a
qkforest <89928584+qkforest@users.noreply.github.com>
2025-07-29 12:29:50 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.07 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,66 +1,68 @@
-import java.util.*;
 class Solution {
-    public int[][] directions = {{1, 0}, {0, 1}, {2, 0}, {0, 2}, {1, 1}, {1, -1}};
-    public int[] solution(String[][] places) {
-        int len = places.length;
-        int[] answer = new int[len];
-        Arrays.fill(answer, 1);
-        for(int i = 0; i < len; i++){
-            char[][] place = new char[5][5];
-            boolean P = false;
-            for(int j = 0; j < 5; j++){
-                place[j] = places[i][j].toCharArray();
+    private static final int[] dy = {0, -1, 1, 0};
+    private static final int[] dx = {-1, 0, 0, 1};
+    
+    private boolean checkNeighbor(char[][] room, int y, int x, int pass) {
+        for(int i = 0; i < 4; i++) {
+            if(i == pass) {
+                continue;
             }
-            for(int j = 0; j < 5; j++){
-                for(int k = 0; k < 5; k++){
-                    if(place[j][k] == 'P'){
-                        for(int l = 0; l < 6; l++){
-                            int r = j+directions[l][0], c = k+directions[l][1];
-                            if((r >= 0 && r < 5) && (c >= 0 && c < 5)){
-                                if(place[r][c] == 'P'){
-                                    if(l < 2){
-                                        P = true;
-                                        break;
-                                    }
-                                    else if(l == 2){
-                                        if(place[r-1][c] != 'X'){
-                                            P = true;
-                                            break;
-                                        }
-                                    }
-                                    else if(l == 3){
-                                        if(place[r][c-1] != 'X'){
-                                            P = true;
-                                            break;
-                                        }
-                                    }
-                                    else if(l == 4){
-                                        if(place[r-1][c] != 'X' || place[r][c-1] != 'X'){
-                                            P = true;
-                                            break;
-                                        }
-                                    }
-                                     else if(l == 5){
-                                        if(r > 0){
-                                            if(place[r-1][c] != 'X' || place[r][c+1] != 'X'){
-                                                P = true;
-                                                break;
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if(ny < 0 || ny >= 5 || nx < 0 || nx >= 5) {
+                continue;
+            }
+            if(room[ny][nx] == 'P') {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private boolean isDistanced(char[][] room, int y, int x) {      
+        for(int i = 0; i < 4 ; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if(ny < 0 || ny >= 5 || nx < 0 || nx >= 5) {
+                continue;
+            }
+            switch(room[ny][nx]) {
+                case 'P' :
+                    return false;
+                case 'O' :
+                    if(checkNeighbor(room, ny ,nx, 3-i)) {
+                        return false;
                     }
-                    if(P){
-                        break;
-                    }
-                }
-                if(P){
                     break;
+            }
+        }
+        return true;
+    }
+    
+    private boolean isDistanced(char[][] room) {
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < 5; j++) {
+                if(room[i][j] != 'P') {
+                    continue;
+                }
+                if(!isDistanced(room, i, j)) {
+                    return false;
                 }
             }
-            if(P){
+        }
+        return true;
+    }
+    
+    public int[] solution(String[][] places) {
+        int[] answer = new int[places.length];
+        for(int i = 0; i < places.length; i++) {
+            char[][] room = new char[5][];
+            for(int j = 0; j < 5; j++) {
+                room[j] = places[i][j].toCharArray();
+            }
+            if(isDistanced(room)) {
+                answer[i] = 1;
+            } else {
                 answer[i] = 0;
             }
         }
