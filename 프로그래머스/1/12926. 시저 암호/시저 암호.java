a330c97628f1ef02c90a4d0658ce9f99e8dd38cb
qkforest <89928584+qkforest@users.noreply.github.com>
2025-07-29 14:02:53 +0900

    [level 1] Title: 시저 암호, Time: 1.55 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,25 +1,17 @@
 class Solution {
     public String solution(String s, int n) {
-        String answer = "";
-        char ch;
-        for(char c : s.toCharArray()){
-            if(c == ' ')
-                answer += ' ';
-            else{
-                if(c >= 'a' && c <= 'z'){
-                    if(c + n > 'z')
-                        ch = (char)(c + (n - 25 -1));
-                    else
-                        ch = (char)(c + n);
-                } else {
-                    if(c + n > 'Z')
-                        ch = (char)(c + (n - 25 -1));
-                    else
-                        ch = (char)(c + n);
-                }
-                answer += String.valueOf(ch);
-            }  
+        StringBuilder answer = new StringBuilder();
+        for(char a : s.toCharArray()) {
+            if(a == ' ') {
+                answer.append(a);
+                continue;
+            }
+            int c = a + n;
+            if((Character.isLowerCase(a) && c > 'z') || (Character.isUpperCase(a) && c > 'Z')) {
+                 c -= 26;
+            }
+            answer.append((char)c);
         }
-        return answer;
+        return answer.toString();
     }
 }
