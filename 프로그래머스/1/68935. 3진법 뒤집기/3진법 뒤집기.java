332bf6f44e815db93a6a47c02dd3d5cf44438dba
qkforest <89928584+qkforest@users.noreply.github.com>
2025-07-30 10:04:12 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.09 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,16 +1,6 @@
-import java.util.*;
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        List<Integer> r3 = new ArrayList<>();
-        while(n!=0){
-            r3.add(n%3);
-            n /= 3;
-        }
-        String ans = "";
-        for(int i = 0; i < r3.size(); i++){
-            ans += r3.get(i);
-        }
-        return Integer.parseInt(ans, 3);
+        StringBuilder sb = new StringBuilder(Integer.toString(n, 3));
+        return Integer.parseInt(sb.reverse().toString(), 3);
     }
 }
