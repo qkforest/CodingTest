988b14e8e5f5053fbc4b70f65c19f1a39f016cb5
qkforest <89928584+qkforest@users.noreply.github.com>
2025-08-05 13:22:17 +0900

    [level 2] Title: 모음 사전, Time: 33.76 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,26 +1,19 @@
 import java.util.*;
 class Solution {
-    public String[] alphabets = {"A", "E", "I", "O", "U"};
-    public List<String> list;
-    public int solution(String word) {
-        list = new ArrayList<>();
-        int answer = 0;
-        dfs("", 0);
-        for(int i = 0; i < list.size(); i++) {
-            if(list.get(i).equals(word)){
-                answer = i;
-                break;
-            }
-        }
-        return answer;
-    }
-    public void dfs(String s, int len) {
-        list.add(s);
-        if(len == 5){
+    private final char[] alphabets = "AEIOU".toCharArray();
+    private static List<String> dict = new ArrayList<>();
+    private void generate(String temp, int len) {
+        if(len == 5) {
             return;
         }
         for(int i = 0; i < 5; i++) {
-            dfs(s+alphabets[i], len+1);
+            String next = temp + alphabets[i];
+            dict.add(next);
+            generate(next, len+1);
         }
+    }
+    public int solution(String word) {
+        generate("", 0);
+        return dict.indexOf(word)+1;
     }
 }
