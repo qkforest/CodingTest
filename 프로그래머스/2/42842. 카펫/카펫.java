c2ee6f3fe9820aaf0116be5ddb957e1d9a7e660e
qkforest <89928584+qkforest@users.noreply.github.com>
2025-08-05 14:41:12 +0900

    [level 2] Title: 카펫, Time: 0.06 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,11 +1,11 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        for(int w = 3; w <= 5000; w++) {
-            for(int h = 3; h <= w; h++) {
-                int b = (w+h-2) * 2;
-                if(b == brown && w*h - b == yellow) {
-                    return new int[]{w, h};
-                } 
+        for(int h = 1; h <= yellow; h++) {
+            if(yellow % h != 0) {
+                continue;
+            }
+            if(2*(h+yellow/h)+4 == brown) {
+                return new int[] {yellow/h+2, h+2};
             }
         }
         return null;
