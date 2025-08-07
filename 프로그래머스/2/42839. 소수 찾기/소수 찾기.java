f62a2474101445b32c71067d92b11fc49ae9c13d
qkforest <89928584+qkforest@users.noreply.github.com>
2025-08-07 13:16:07 +0900

    [level 2] Title: 소수 찾기, Time: 9.99 ms, Memory: 91 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,48 +1,50 @@
 import java.util.*;
+
 class Solution {
-    public static char[] numArray, temp;
-    public static boolean[] visited;
-    public static List<Integer> answer;
-    public static int len;
+    private static Set<Integer> set;
+    private static char[] num;
+    private static boolean[] visited;
+    private static int len;
+    
+    private void dfs(int level, StringBuilder sb) {
+        if(level == len) {
+            return;
+        }
+        for(int i = 0; i < len; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                sb.append(num[i]);
+                int n = Integer.parseInt(sb.toString());
+                if(isPrime(n)){
+                    set.add(n);
+                }
+                dfs(level+1, sb);
+                sb.deleteCharAt(level);
+                visited[i] = false;
+            }
+        }
+    }
+    
+    public boolean isPrime(int n) {
+        if(n == 1 || n == 0) {
+            return false;
+        }
+        
+        for(int i = 2; i <= Math.sqrt(n); i++) {
+            if(n % i == 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
     
     public int solution(String numbers) {
-        numArray = numbers.toCharArray();
-        answer = new ArrayList<>();
-        len = numArray.length;
-        temp = new char[len];
+        set = new HashSet<>();
+        num = numbers.toCharArray();
+        len = num.length;
         visited = new boolean[len];
-        
-        for(int i = 1; i <= len; i++) dfs(0, i);
-        
-        return answer.size();
-    }
-    
-    public void dfs(int level, int size){
-        if(level == size){
-            String s = "";
-            for(char c : temp){
-                if(c != 0) s += c;
-            }
-            int n = Integer.parseInt(s);
-            if(!answer.contains(n) && n > 1){
-                if(check(n)) answer.add(n);
-            }
-        } else {
-            for(int i = 0; i < len; i++){
-                if(!visited[i]){
-                    visited[i] = true;
-                    temp[level] = numArray[i];
-                    dfs(level+1, size);
-                    visited[i] = false;
-                }
-            }
-        }
-    }
-    
-    public boolean check(int n){
-        for(int i = 2; i < n; i++){
-            if(n % i == 0) return false;
-        }
-        return true;
+        dfs(0, new StringBuilder());
+        return set.size();
     }
 }
