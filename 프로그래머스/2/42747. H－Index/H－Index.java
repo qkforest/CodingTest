e9826b2180c137a83a740c34db875ba7629a6e7b
qkforest <89928584+qkforest@users.noreply.github.com>
2025-08-12 11:37:41 +0900

    [level 2] Title: H-Index, Time: 1.18 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,17 +1,32 @@
 import java.util.*;
-
 class Solution {
-    public int solution(int[] citations) {
-        int answer = 0;
-        Arrays.sort(citations);
-        
-        for(int i = 0; i < citations.length; i++){
-            if(citations[i] >= citations.length-i){
-                answer = citations.length-i;
-                break;
-            }     
+    private int n;
+    private boolean count(int h, int[] citations) {
+        int c = 0;
+        while(c < n && citations[n-c-1] >= h) {
+            c++;
         }
-        
+        if(c >= h) {
+            return true;   
+        }
+        return false;
+    }
+    
+    public int solution(int[] citations) {
+        n = citations.length;
+        int answer = 0;
+        int left = 0;
+        int right = n;
+        Arrays.sort(citations);
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if(count(mid, citations)) {
+                answer = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
         return answer;
     }
 }
