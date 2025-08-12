9f70f9804e3fe6b1c9c4741dca56b4f2118b2099
qkforest <89928584+qkforest@users.noreply.github.com>
2025-08-12 15:08:46 +0900

    [level 2] Title: 가장 큰 수, Time: 201.95 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,23 +1,16 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 class Solution {
     public String solution(int[] numbers) {
-        String answer = "";
-        String[] arr = new String[numbers.length];
-        
-        for(int i = 0; i < numbers.length; i++){
-            arr[i] = String.valueOf(numbers[i]);
-        }
-        
-        Arrays.sort(arr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
-        
-        if(arr[0].equals("0"))
-            answer = "0";
-        else {
-            for(String x : arr)
-                answer = answer + x;
-        }
-            
-        return answer;
+        return Arrays.stream(numbers)
+            .mapToObj(String::valueOf)
+            .sorted((s1, s2) -> {
+                int a = Integer.parseInt(s1+s2);
+                int b = Integer.parseInt(s2+s1);
+                return b - a;
+            })
+        .collect(Collectors.joining())
+        .replaceAll("^0+", "0");
     }
 }
