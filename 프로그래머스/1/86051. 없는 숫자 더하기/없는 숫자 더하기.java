a29c7c013521045fba071c4baeab8bee801dc901
qkforest <89928584+qkforest@users.noreply.github.com>
2025-08-18 16:36:55 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,11 +1,8 @@
-import java.util.*;
 class Solution {
     public int solution(int[] numbers) {
         int answer = 45;
-        Integer[] all = new Integer[] {1,2,3,4,5,6,7,8,9,0};
-        for(int n : numbers){
-            if(Arrays.asList(all).contains(n))
-                answer -= n;
+        for(int n : numbers) {
+            answer -= n;
         }
         return answer;
     }
