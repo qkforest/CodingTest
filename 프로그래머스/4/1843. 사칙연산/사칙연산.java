5c2dc79555c537b3da89d6e5a9e93bce74920335
qkforest <89928584+qkforest@users.noreply.github.com>
2025-08-19 15:42:42 +0900

    [level 4] Title: 사칙연산, Time: 15.19 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/4/1843. 사칙연산/사칙연산.java
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.java
@@ -1,46 +1,39 @@
-import java.util.*;
 class Solution {
     public int solution(String arr[]) {
-        char[] op = new char[arr.length/2];
-        int[][][] dp = new int[arr.length/2+1][arr.length/2+1][2]; 
-        int idx1 = 0;
-        int idx2 = 0;
+        int length = arr.length / 2;
+        char[] op = new char[length];
+        int[][][] dp = new int[length+1][length+1][2];
+        int o = 0;
+        int n = 0;
         for(String s : arr) {
-            if(s.equals("+")) {
-                op[idx2++] = '+';
-            }
-            else if(s.equals("-")) {
-                op[idx2++] = '-';
-            }
-            else {
-                int n = Integer.parseInt(s);
-                dp[idx1][idx1][0] = n;
-                dp[idx1][idx1][1] = n;
-                idx1++;
+            if(s.equals("+") || s.equals("-")) {
+                op[o++] = s.charAt(0);
+            } else {
+                dp[n][n][0] = Integer.parseInt(s);
+                dp[n][n++][1] = Integer.parseInt(s);
             }
         }
-        for(int len = 1; len < arr.length/2+1; len++) {
-            for(int start = 0; start < arr.length/2+1; start++) {
-                if(start + len >= arr.length/2+1) {
+        for(int len = 1; len <= length; len++) {
+            for(int start = 0; start <= length; start++) {
+                if(len + start >= length + 1) {
                     break;
                 }
                 int end = start + len;
-                int max = Integer.MIN_VALUE;
                 int min = Integer.MAX_VALUE;
-                for (int i = start; i < end; i++) {
-                    if (op[i] == '+') {
-                        max = Math.max(max, dp[start][i][1] + dp[i+1][end][1]);
-                        min = Math.min(min, dp[start][i][0] + dp[i+1][end][0]);
-                    } 
-                    else {
-                        max = Math.max(max, dp[start][i][1] - dp[i+1][end][0]);
-                        min = Math.min(min, dp[start][i][0] - dp[i+1][end][1]);
+                int max = Integer.MIN_VALUE;
+                for(int i = start; i < end; i++) {
+                    if(op[i] == '+') {
+                        min = Math.min(min, dp[start][i][0]+dp[i+1][end][0]);
+                        max = Math.max(max, dp[start][i][1]+dp[i+1][end][1]);
+                    } else {
+                        min = Math.min(min, dp[start][i][0]-dp[i+1][end][1]);
+                        max = Math.max(max, dp[start][i][1]-dp[i+1][end][0]);
                     }
                 }
                 dp[start][end][0] = min;
                 dp[start][end][1] = max;
             }
         }
-        return dp[0][arr.length/2][1];
+        return dp[0][length][1];
     }
 }
