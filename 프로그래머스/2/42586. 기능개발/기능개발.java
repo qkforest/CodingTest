89cff9ba5c1a5385a7e52e0d53febd60c4e6927a
qkforest <89928584+qkforest@users.noreply.github.com>
2025-08-20 13:42:31 +0900

    [level 2] Title: 기능개발, Time: 2.08 ms, Memory: 93.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,20 +2,23 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         List<Integer> answer = new ArrayList<>();
-        for(int i = 0; i < progresses.length; i++)
-            progresses[i] = (int)Math.ceil((100 - progresses[i])/(float)speeds[i]);
-        int count = 1, day = progresses[0];
-        for(int i = 1; i < progresses.length; i++){
-            if(progresses[i] <= day)
+        int count = 1;
+        int time = (100 - progresses[0]) / speeds[0];
+        time += ((100 - progresses[0]) % speeds[0] != 0) ? 1 : 0;
+        for(int i = 1; i < speeds.length; i++) {
+            int t = (100 - progresses[i]) / speeds[i];
+            t += ((100 - progresses[i]) % speeds[i] != 0) ? 1 : 0;
+            if(t <= time) {
                 count++;
-            else{
+            } else {
                 answer.add(count);
-                day = progresses[i];
                 count = 1;
+                time = t;
             }
         }
-        if(count > 0)
+        if(count > 0) {
             answer.add(count);
+        }
         return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
