eaa5c1381c135db2f4d02b80afd5befaa1cf759b
qkforest <89928584+qkforest@users.noreply.github.com>
2025-08-22 15:45:33 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.04 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,29 +1,30 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[][] jobs) {
         int answer = 0;
         int time = 0;
         
-        PriorityQueue<int[]> jobsQ = new PriorityQueue<>((o1, o2) -> o1[0] - o2[0]);
-        PriorityQueue<int[]> readyQ = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+        PriorityQueue<int[]> jobQueue = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        PriorityQueue<int[]> readyQueue = new PriorityQueue<>((a, b) -> a[1] - b[1]);
         
-        for(int i = 0; i < jobs.length; i++){
-            jobsQ.offer(jobs[i]);
+        for(int[] job : jobs) {
+            jobQueue.offer(job);
         }
         
-        while(!jobsQ.isEmpty() || !readyQ.isEmpty()){
-            while(!jobsQ.isEmpty() && jobsQ.peek()[0] <= time){
-                readyQ.offer(jobsQ.poll());
+        while(!jobQueue.isEmpty() || !readyQueue.isEmpty()) {
+            while(!jobQueue.isEmpty() && jobQueue.peek()[0] <= time) {
+                readyQueue.offer(jobQueue.poll());
             }
-            if(readyQ.isEmpty()){
-                time = jobsQ.peek()[0];
-            }
-            else{
-                int[] work = readyQ.poll();
-                answer += (time+work[1]-work[0]);
-                time += work[1];
+            if(readyQueue.isEmpty()) {
+                time = jobQueue.peek()[0];
+            } else {
+                int[] job = readyQueue.poll();
+                time += job[1];
+                answer += time - job[0];
             }
         }
-        return answer/jobs.length;
+        
+        return answer / jobs.length;
     }
 }
