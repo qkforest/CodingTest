99c9a5f9853112f516b22e48028896230b069b20
qkforest <89928584+qkforest@users.noreply.github.com>
2025-08-22 11:44:12 +0900

    [level 3] Title: 길 찾기 게임, Time: 35.52 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
@@ -1,63 +1,81 @@
 import java.util.*;
+
 class Solution {
-    static int idx;
-    static int[][] answer;
-    public int[][] solution(int[][] nodeinfo) {
-        int num = nodeinfo.length;
-        answer = new int[2][num];
-        Node[] nodes = new Node[num];
-        for(int i = 0; i < num; i++)
-            nodes[i] = new Node(i+1, nodeinfo[i][0], nodeinfo[i][1], null, null);
-        Arrays.sort(nodes, (a, b) -> a.y == b.y ? a.x - b.x : b.y - a.y);
-        Node parent = nodes[0];
-        for(int i = 1; i < num; i++)
-            constructTree(parent, nodes[i]);
-        idx = 0;
-        preOrder(parent);
-        idx = 0;
-        postOrder(parent); 
-        return answer;
-    }
-    class Node {
-        int value;
-        int x;
-        int y;
-        Node left;
-        Node right;
+    private static int len;
+    private static class Node {       
+        public final int value;
+        public final int x;
+        public final int y;
         
-        public Node(int value, int x, int y, Node left, Node right){
+        public Node left;
+        public Node right;
+        
+        private Node(int value, int x, int y) {
             this.value = value;
             this.x = x;
             this.y = y;
-            this.left = left;
-            this.right = right;
         }
     }
-    public void constructTree(Node parent, Node child){
-        if(parent.x >= child.x){
-            if(parent.left == null)
-                parent.left = child;
-            else
-                constructTree(parent.left, child);
+    private Node constructTree(Node[] nodes) {
+        Node root = nodes[0];
+        
+        for(int i = 1; i < len; i++) {
+            insert(root, nodes[i]);
+        }
+        
+        return root;
+    }
+    
+    private void insert(Node root, Node node) {
+        if(node.x < root.x) {
+            if(root.left == null) {
+                root.left = node;
+            } else {
+                insert(root.left, node);
+            }
         } else {
-            if(parent.right == null)
-                parent.right = child;
-            else
-                constructTree(parent.right, child);
+            if(root.right == null) {
+                root.right = node;
+            } else {
+                insert(root.right, node);
+            }
         }
     }
-    public void preOrder(Node root){
-       if(root != null){
-            answer[0][idx++] = root.value;
-            preOrder(root.left);
-            preOrder(root.right);
+    
+    private void preOrder(Node node, List<Integer> visit) {
+        visit.add(node.value);
+        if(node.left != null) {
+            preOrder(node.left, visit);
+        }
+        if(node.right != null) {
+            preOrder(node.right, visit);
         }
     }
-    public void postOrder(Node root){
-       if(root != null){
-           postOrder(root.left);
-           postOrder(root.right);
-           answer[1][idx++] = root.value;
+    
+    private void postOrder(Node node, List<Integer> visit) {
+        if(node.left != null) {
+            postOrder(node.left, visit);
         }
+        if(node.right != null) {
+            postOrder(node.right, visit);
+        }
+        visit.add(node.value);
+    }
+    
+    
+    public int[][] solution(int[][] nodeinfo) {
+        len = nodeinfo.length;
+        Node[] nodes = new Node[len];
+        for(int i = 0; i < len; i++) {
+            nodes[i] = new Node(i+1, nodeinfo[i][0], nodeinfo[i][1]);
+        }
+        Arrays.sort(nodes, (a, b) -> b.y - a.y);
+        Node root = constructTree(nodes);
+        List<Integer> preOrder = new ArrayList<>();
+        List<Integer> postOrder = new ArrayList<>();
+        preOrder(root, preOrder);
+        postOrder(root, postOrder);
+        return new int[][] {preOrder.stream().mapToInt(Integer::intValue).toArray(),
+            postOrder.stream().mapToInt(Integer::intValue).toArray()};
     }
 }
