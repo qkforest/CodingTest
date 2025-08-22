c69af24133e3b81110049ded5cd572d4a8b5462f
qkforest <89928584+qkforest@users.noreply.github.com>
2025-08-22 14:45:33 +0900

    [level 3] Title: 이중우선순위큐, Time: 109.56 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -3,31 +3,38 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] operations) {
         int[] answer = new int[2];
-        List<Integer> list = new ArrayList<>();
-        
+        int size = 0;
+        PriorityQueue<Integer> min = new PriorityQueue<>();
+        PriorityQueue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
         for(String o : operations) {
             String[] arr = o.split(" ");
-            if(arr[0].equals("I")) { 
-                list.add(Integer.parseInt(arr[1]));
+            int n = Integer.parseInt(arr[1]);
+            if(arr[0].equals("I")) {
+                min.offer(n);
+                max.offer(n);
+                size++;
             } else {
-                if(list.isEmpty()){
-                    continue;
-                }
-                Collections.sort(list);
-                if(arr[1].equals("-1")) {
-                    list.remove(0);
-                } else {
-                    list.remove(list.size()-1);
+                if(size != 0) {
+                    if(n == 1) {
+                        int m = max.poll();
+                        min.remove(m);
+                    } else {
+                        int m = min.poll();
+                        max.remove(m);
+                    }
+                    size--;
+                    if(size == 0) {
+                        min.clear();
+                        max.clear();
+                    }
                 }
             }
         }
         
-        if(!list.isEmpty()){
-            Collections.sort(list);
-            answer[0] = list.get(list.size()-1);
-            answer[1] = list.get(0); 
+        if(size != 0) {
+            answer[0] = max.poll();
+            answer[1] = min.poll();
         }
-        
         return answer;
     }
 }
