57920d6a3ca6dfda93560559f9537862ac58e15d
qkforest <89928584+qkforest@users.noreply.github.com>
2025-08-25 11:45:32 +0900

    [level 3] Title: 섬 연결하기, Time: 0.82 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,38 +1,45 @@
 import java.util.*;
+
 class Solution {
-    public int[] parents;
+    private static int[] parents;
+    
+    private int find(int a) {
+        if(parents[a] == a) {
+            return a;
+        }
+        return parents[a] = find(parents[a]);
+    }
+    
+    private boolean union(int a, int b) {
+        int parentA = find(a);
+        int parentB = find(b);
+        
+        if(parentA == parentB) {
+            return false;
+        }
+        
+        if(parentA < parentB) {
+            parents[parentB] = parentA;
+        } else {
+            parents[parentA] = parentB;
+        }
+        
+        return true;
+    }
     public int solution(int n, int[][] costs) {
-        int answer = 0;
         parents = new int[n];
-        for(int i = 0; i < n; i++) {
+        for(int i = 1; i < n; i++) {
             parents[i] = i;
         }
+        
+        int answer = 0;
         Arrays.sort(costs, (a, b) -> a[2] - b[2]);
         for(int i = 0; i < costs.length; i++) {
-            if(union(costs[i][0], costs[i][1])){
+            if(union(costs[i][0], costs[i][1])) {
                 answer += costs[i][2];
             }
         }
+        
         return answer;
-    }
-    public boolean union(int a, int b){
-        int aParent = find(a);
-        int bParent = find(b);
-        if(aParent == bParent) {
-            return false;
-        }
-        if(aParent < bParent) {
-            parents[bParent] = aParent;
-        } 
-        else {
-            parents[aParent] = bParent;
-        }
-        return true;
-    }
-    public int find(int n) {
-        if(parents[n] == n) {
-            return n;
-        }
-        return parents[n] = find(parents[n]);
     }
 }
