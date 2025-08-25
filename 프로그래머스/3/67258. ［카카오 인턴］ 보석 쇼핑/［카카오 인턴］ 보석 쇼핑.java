eca2fced786b12db9dbb4154d94e27c8e13fd86e
qkforest <89928584+qkforest@users.noreply.github.com>
2025-08-25 10:48:24 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 82.79 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -1,37 +1,31 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(String[] gems) {
-        int[] answer = new int[2];
-        Set<String> category = new HashSet<>();
+        int start = 0;
+        int end = gems.length - 1;
+        Set<String> gemSet = new HashSet<>(List.of(gems));
         Map<String, Integer> counts = new HashMap<>();
-        for(int i = 0; i < gems.length; i++) category.add(gems[i]);
-        if(category.size() == 1) return new int[] {1, 1};
-        int end = 0, start = 0;
-        for(int i = 0; i < gems.length; i++){
-            if(counts.size() != category.size()){
-                if(!counts.containsKey(gems[i])) {
-                    counts.put(gems[i], 1);
-                    end = i;
-                } else counts.put(gems[i], counts.get(gems[i])+1);
-            } else break;
-        }
-        int dis = end - start + 1;
-        answer[0] = start+1;
-        answer[1] = end+1;
-        for(int i = end; i < gems.length; i++){
-            while(i - start + 1 >= category.size()){
-                if(counts.get(gems[start]) >= 2) {
-                    counts.put(gems[start], counts.get(gems[start])-1);
-                    start++;
-                } else break;
+        int s = 0;
+        int e = 0;
+        counts.put(gems[s], 1);
+                
+        while(s < gems.length) {
+            if(counts.keySet().size() == gemSet.size()) {
+                if(e - s < end - start) {
+                    end = e;
+                    start = s;
+                }
+                if(counts.merge(gems[s], -1, Integer::sum) == 0) {
+                    counts.remove(gems[s]);
+                }
+                s++;
+            } else if(e < gems.length - 1) {
+                counts.merge(gems[++e], 1, Integer::sum);
+            } else {
+                break;
             }
-            if(i - start + 1 < dis){
-                dis = i - start + 1;
-                answer[0] = start+1;
-                answer[1] = i+1;
-            }
-            if(i < gems.length - 1) counts.put(gems[i+1], counts.get(gems[i+1])+1);
         }
-        return answer;
+        return new int[] {start+1, end+1};
     }
 }
