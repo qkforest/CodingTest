3f9a7b1f399c8b3b807d3316504762178cc012d4
qkforest <89928584+qkforest@users.noreply.github.com>
2025-08-26 12:10:21 +0900

    [level 4] Title: 호텔 방 배정, Time: 399.17 ms, Memory: 153 MB -BaekjoonHub

--- 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.java
+++ 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.java
@@ -1,26 +1,23 @@
 import java.util.*;
 
 class Solution {
-    private long find(Map<Long, Long> parents, long room) {
-        if(!parents.containsKey(room)) {
-            parents.put(room, room+1);
+    private static Map<Long, Long> rooms;
+    private static long assignRoom(long room) {
+        if(!rooms.containsKey(room)) {
+            rooms.put(room, room+1);
             return room;
         }
-        
-        long next = find(parents, parents.get(room));
-        parents.put(room, next);
+        long next = assignRoom(rooms.get(room));
+        rooms.put(room, next);
         return next;
     }
     public long[] solution(long k, long[] room_number) {
         int len = room_number.length;
         long[] answer = new long[len];
-        
-        Map<Long, Long> parents = new HashMap<>();
-        
+        rooms = new HashMap<>();
         for(int i = 0; i < len; i++) {
-            answer[i] = find(parents, room_number[i]); 
+            answer[i] = assignRoom(room_number[i]);
         }
-        
         return answer;
     }
 }
