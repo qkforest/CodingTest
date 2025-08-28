f28044f88df3482c7eb15639b52fa303b9de1c48
qkforest <89928584+qkforest@users.noreply.github.com>
2025-08-28 14:32:59 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.19 ms, Memory: 54.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,36 +1,36 @@
 import java.util.*;
 
 class Solution {
-    static int[][] d = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    public int solution(int[][] maps) {
-        int n = maps.length, m = maps[0].length;
-        int[][] visited = new int[n][m];
-        bfs(maps, visited, n, m);
-        int answer = visited[n-1][m-1];
-        return (answer > 0) ? answer : -1;
-    }
-    public static void bfs(int[][] maps, int[][] visited, int n, int m){
-        int x = 0, y = 0;
-        visited[x][y] = 1;
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{y, x});
+    private static final int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    
+    private int bfs(int[][] maps) {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[] {0, 0, 1});
+        int n = maps.length;
+        int m = maps[0].length;
+        boolean[][] visited = new boolean[n][m];
+        visited[0][0] = true;
         
-        while(!queue.isEmpty()){
-            int[] now = queue.poll();
-            int r = now[0];
-            int c = now[1];
-            
-            for(int i = 0; i < 4; i++){
-                int dr = r + d[i][0];
-                int dc = c + d[i][1];
-                
-                if(dr < 0 || dr >= n || dc < 0 || dc >= m)
+        while(!q.isEmpty()) {
+            int[] now = q.poll();
+            if(now[0] == n-1 && now[1] == m-1) {
+                return now[2];
+            }
+            for(int[] d : directions) {
+                int ny = now[0] + d[0];
+                int nx = now[1] + d[1];
+                if(ny < 0 || ny >= n || nx < 0 || nx >= m || visited[ny][nx] || maps[ny][nx] == 0) {
                     continue;
-                if(visited[dr][dc] == 0 && maps[dr][dc] == 1){
-                    queue.add(new int[]{dr, dc});
-                    visited[dr][dc] = visited[r][c] + 1;
                 }
+                visited[ny][nx] = true;
+                q.offer(new int[] {ny, nx, now[2]+1});
             }
         }
+        
+        return -1;
+    } 
+    
+    public int solution(int[][] maps) {
+        return bfs(maps);
     }
 }
