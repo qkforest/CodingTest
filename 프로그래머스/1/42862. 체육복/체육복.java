4380d35f92a78940c651cafeb27ecbaae0403c28
qkforest <89928584+qkforest@users.noreply.github.com>
2025-08-28 15:41:13 +0900

    [level 1] Title: 체육복, Time: 0.66 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,32 +1,38 @@
 import java.util.*;
+
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = n - lost.length;
-        Set<Integer> r = new HashSet<>();
-        boolean s;
-        for(int i = 0; i < reserve.length; i++){
-            r.add(reserve[i]);
+        PriorityQueue<Integer> lostQueue = new PriorityQueue<>();
+        PriorityQueue<Integer> reserveQueue = new PriorityQueue<>();
+        
+        for(int r : reserve){
+            reserveQueue.offer(r);
         }
-        for(int i = 0; i < lost.length; i++){
-            if(r.contains(lost[i])){
-                answer += 1;
-                r.remove(lost[i]);
-                lost[i] = -20;
+        
+        for(int i = 0; i < lost.length; i++) {
+            if(reserveQueue.contains(lost[i])) {
+                reserveQueue.remove(lost[i]);
+                answer++;
+                continue;
+            }
+            lostQueue.offer(lost[i]);
+        }
+        
+        while(!lostQueue.isEmpty()) {
+            int l = lostQueue.poll();
+            while(!reserveQueue.isEmpty() && l - reserveQueue.peek() > 1) {
+                reserveQueue.poll();
+            }
+            if(reserveQueue.isEmpty()) {
+                break;
+            }
+            if(Math.abs(reserveQueue.peek() - l) <= 1) {
+                reserveQueue.poll();
+                answer++;
             }
         }
-        Arrays.sort(lost);
-        for(int i = 0; i < lost.length; i++){
-            s = true;
-            if(lost[i] - 1 > 0 && r.contains(lost[i]-1)){
-                answer += 1;
-                r.remove(lost[i]-1);
-                s = false;
-            }
-            if(s && lost[i] + 1 > 0 && lost[i] + 1 <= n && r.contains(lost[i]+1)){
-                answer += 1;
-                r.remove(lost[i]+1);
-            }
-        }
+        
         return answer;
     }
 }
