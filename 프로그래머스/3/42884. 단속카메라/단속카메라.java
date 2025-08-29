a20a33e979c523b890dd479a50c2fe8239590062
qkforest <89928584+qkforest@users.noreply.github.com>
2025-08-29 14:24:49 +0900

    [level 3] Title: 단속카메라, Time: 6.82 ms, Memory: 55.6 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,16 +1,15 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[][] routes) {
         int answer = 1;
         Arrays.sort(routes, (a, b) -> a[1] - b[1]);
         int camera = routes[0][1];
-        //System.out.println(Arrays.deepToString(routes));
-        for(int i = 1; i < routes.length; i++) {
-            if(routes[i][0] <= camera) {
-                continue;
+        for(int[] r : routes) {
+            if(r[0] > camera) {
+                answer++;
+                camera = r[1];
             }
-            camera = routes[i][1];
-            answer++;
         }
         return answer;
     }
