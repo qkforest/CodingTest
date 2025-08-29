7f585f8dba2c15f56cfc1c6a46fd2cd926be21e2
qkforest <89928584+qkforest@users.noreply.github.com>
2025-08-29 14:41:22 +0900

    [level 1] Title: 신고 결과 받기, Time: 160.84 ms, Memory: 159 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,22 +1,31 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        int[] rep = new int[id_list.length];
-        String[] rep2 = new String[id_list.length];
+        Map<String, Set> map = new HashMap<>();
+        Map<String, Integer> idx = new HashMap<>();
         int[] answer = new int[id_list.length];
-        Set<String> temp = new HashSet<>(Arrays.asList(report));
-        for(String s : temp){
-            String[] t = s.split(" ");
-            int idx = Arrays.asList(id_list).indexOf(t[1]);
-            rep2[idx] = rep2[idx] + t[0] + ",";
-            rep[idx] += 1;
+        
+        for(int i = 0; i < id_list.length; i++) {
+            idx.put(id_list[i], i);
         }
-        for(int i = 0; i < answer.length; i++){
-            if(rep[i] >= k){
-                for(String s : rep2[i].replaceAll("null","").split(","))
-                    answer[Arrays.asList(id_list).indexOf(s)] += 1;
+        
+        for(String r : report) {
+            String[] arr = r.split(" ");
+            Set<String> list = map.getOrDefault(arr[1], new HashSet<>());
+            list.add(arr[0]);
+            map.put(arr[1], list);
+        }
+        
+        for(String name : map.keySet()) {
+            Set<String> list = map.get(name);
+            if(list.size() >= k) {
+                for(String n : list) {
+                    answer[idx.get(n)]++;
+                }
             }
         }
+        
         return answer;
     }
 }
