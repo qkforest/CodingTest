3a55096f6f7d05e97a35991ffcc670fb129c38c4
qkforest <89928584+qkforest@users.noreply.github.com>
2025-09-01 11:12:51 +0900

    [level 2] Title: 주차 요금 계산, Time: 11.49 ms, Memory: 93.7 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,45 +1,38 @@
 import java.util.*;
+
 class Solution {
+    private static Map<String, Integer> time, inOut;
+    private int count(String car, int[] fees) {
+        int t = time.get(car);
+        int fee = fees[1];
+        t -= fees[0];
+        if(t > 0) {
+            fee += t / fees[2] * fees[3];
+            if(t % fees[2] > 0) {
+                fee += fees[3];
+            }
+        }
+        return fee;
+    }
     public int[] solution(int[] fees, String[] records) {
-        List<Integer> answer = new ArrayList<>();
-        Map<String, String> rec = new HashMap<>();
-        Map<String, Integer> fee = new TreeMap<>();
-        String a = "", b = "";
-        int charge = 0;
-        for(String r : records){
-            if(rec.containsKey(r.substring(6, 10))){
-                a = rec.get(r.substring(6, 10));
-                b = r.substring(0, 5);
-                charge = Integer.valueOf(b.substring(3, 5)) - Integer.valueOf(a.substring(3, 5));
-                if(charge > 0)
-                    charge += (Integer.valueOf(b.substring(0, 2)) - Integer.valueOf(a.substring(0, 2))) * 60;
-                else{
-                    charge += 60;
-                    charge += (Integer.valueOf(b.substring(0, 2)) - 1 - Integer.valueOf(a.substring(0, 2))) * 60;
-                }
-                if(fee.containsKey(r.substring(6, 10)))
-                    fee.put(r.substring(6, 10), fee.get(r.substring(6, 10))+charge);
-                else
-                    fee.put(r.substring(6, 10), charge);
-                rec.remove(r.substring(6, 10));
-            } else
-                rec.put(r.substring(6, 10), r.substring(0, 5));
+        time = new HashMap<>();
+        inOut = new HashMap<>();
+        for(String r : records) {
+            String[] arr = r.split(" ");
+            String[] t = arr[0].split(":");
+            int n = Integer.parseInt(t[0]) * 60 + Integer.parseInt(t[1]);
+            if(inOut.containsKey(arr[1])) {
+                time.merge(arr[1], n - inOut.get(arr[1]), Integer::sum);
+                inOut.remove(arr[1]);
+            } else {
+                inOut.put(arr[1], n);
+            }
         }
-        for (String key : rec.keySet()) {
-            a = rec.get(key);
-            charge = 59 - Integer.valueOf(a.substring(3, 5));
-            charge += (23 - Integer.valueOf(a.substring(0, 2))) * 60;
-            if(fee.containsKey(key))
-                fee.put(key, fee.get(key)+charge);
-            else
-                fee.put(key, charge);
-        }
-        for (String key : fee.keySet()){
-            charge = fees[1];
-            if(fee.get(key) - fees[0] > 0)
-                charge += fees[3] * (int) Math.ceil((fee.get(key) - fees[0])/(double)fees[2]);
-            answer.add(charge);
-        }
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        for(String car : inOut.keySet()) {
+            time.merge(car, 1439 - inOut.get(car), Integer::sum);
+        } 
+        List<String> list = new ArrayList<>(time.keySet());
+        Collections.sort(list);
+        return list.stream().mapToInt(car -> count(car, fees)).toArray();
     }
 }
