1413092531e5b048f77f17bdb4cff55fbdaf45cc
qkforest <89928584+qkforest@users.noreply.github.com>
2025-10-08 23:39:05 +0900

    [level 0] Title: 외계행성의 나이, Time: 13.86 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(int age) {
+        String answer = "";
+        for(int i = 0; i < String.valueOf(age).length(); i++)
+            answer += (char)((int)"a".charAt(0) + String.valueOf(age).charAt(i) - '0');
+        return answer;
+    }
+}
