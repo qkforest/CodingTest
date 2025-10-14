e8825fe88161aabbf8a7fa97ab4898adf998b8e2
qkforest <89928584+qkforest@users.noreply.github.com>
2025-10-14 23:43:32 +0900

    [level 0] Title: 글자 지우기, Time: 15.75 ms, Memory: 85.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        String answer = "";
+        List<Integer> temp = Arrays.stream(indices).boxed().collect(Collectors.toList());
+        for(int i = 0; i < my_string.length(); i++){
+            if(!(temp.contains(i)))
+                answer +=  my_string.charAt(i);
+        }
+        return answer;
+    }
+}
