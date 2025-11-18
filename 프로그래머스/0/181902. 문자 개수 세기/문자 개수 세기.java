d05a11e66aeaa017be0be796ba95f771c7576f1b
qkforest <89928584+qkforest@users.noreply.github.com>
2025-11-18 23:42:31 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.09 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(String my_string) {
+        int[] answer = new int[52];
+        for(char s: my_string.toCharArray()){
+            if(s - 'a' < 0)
+                answer[(s - 'a')+32] += 1;
+            else
+                answer[(s - 'a')+26] += 1;
+        }
+        
+        return answer;
+    }
+}
