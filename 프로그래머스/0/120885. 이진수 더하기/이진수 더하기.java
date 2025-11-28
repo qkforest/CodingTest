99c8335bb77747fa8696dc859795f344ee95690f
qkforest <89928584+qkforest@users.noreply.github.com>
2025-11-28 22:33:37 +0900

    [level 0] Title: 이진수 더하기, Time: 0.08 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String bin1, String bin2) {
+        int temp = Integer.parseInt(bin1, 2) + Integer.parseInt(bin2, 2);
+        String answer = Integer.toBinaryString(temp);
+        return answer;
+    }
+}
